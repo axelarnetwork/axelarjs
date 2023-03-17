7b24d421bd2184dd09e6d9c54a37da6fb9b80d06
Alan Soares <alanrsoars@gmail.com>
2023-03-18 00:33:21 +1300

    feat(ui): add favicon & manifest

--- apps/maestro/src/pages/_document.tsx
+++ apps/maestro/src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="msapplication-TileColor" content="#050707" />
+        <meta
+          name="msapplication-TileImage"
+          content="/icons/mstile-150x150.png"
+        />
+        <meta name="theme-color" content="#050707" />
+      </Head>
       <body>
         <Main />
         <NextScript />
