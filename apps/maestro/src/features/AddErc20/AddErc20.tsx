e5f139d49168020cac0a498f56754dc5ddbc86bd
Canh Trinh <canh@axelar.network>
2023-03-20 15:13:15 -0400

    chore: set up state file

--- apps/maestro/src/features/AddErc20/AddErc20.tsx
+++ apps/maestro/src/features/AddErc20/AddErc20.tsx
@@ -1,15 +1,17 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { Modal } from "@axelarjs/ui";
 
 type Props = {};
 export const AddErc20: FC<Props> = (props) => {
+  const [userStep, setUserStep] = useState(1);
   return (
     <Modal
       triggerText="Deploy a new ERC-20 token"
       onCancelText="Back"
       onCancel={() => {}}
-      onConfirmText="Next"
+      onConfirmText="Close"
+      cb={() => setUserStep(userStep + 1)}
     />
   );
 };
