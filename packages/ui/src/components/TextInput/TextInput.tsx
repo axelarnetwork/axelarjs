f9cdfe6300503b98da6a60621d743bbf0caed6f6
alanrsoares <alan@axelar.network>
2023-03-21 00:52:58 +1300

    feat: introduce TextInput

---
+++ packages/ui/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,54 @@
+import type { FC } from "react";
+
+import { cva, VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+import tw from "tailwind-styled-components";
+
+const inputVariance = cva("input", {
+  variants: {
+    color: {
+      primary: "input-primary",
+      secondary: "input-secondary",
+      accent: "input-accent",
+      success: "input-success",
+      warning: "input-warning",
+      error: "input-error",
+    },
+    inputSize: {
+      xs: "input-xs",
+      sm: "input-sm",
+      md: "input-md",
+      lg: "input-lg",
+    },
+  },
+});
+
+type VProps = VariantProps<typeof inputVariance>;
+
+const StyledTextInput = tw.input`input`;
+
+StyledTextInput.defaultProps = {
+  type: "text",
+};
+
+export type TextInputProps = JSX.IntrinsicElements["input"] & VProps;
+
+export const TextInput: FC<TextInputProps> = ({
+  color,
+  inputSize,
+  className,
+  ...props
+}) => {
+  return (
+    <StyledTextInput
+      className={twMerge(
+        inputVariance({
+          color,
+          inputSize,
+        }),
+        className
+      )}
+      {...props}
+    />
+  );
+};
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -6,6 +6,7 @@ export * from "./Dropdown";
 export * from "./Footer";
 export * from "./icons";
 export * from "./Navbar";
+export * from "./TextInput";
 export * from "./ThemeSwitcher";
 export * from "./Toggle";
 export * from "./Tooltip";
