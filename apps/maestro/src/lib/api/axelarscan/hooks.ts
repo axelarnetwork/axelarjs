c78329fcf6f26e8ad9a255a35edefa13d3376604
Alan Soares <alanrsoars@gmail.com>
2023-03-20 19:06:24 +1300

    chore: exclude deprecated chains

--- apps/maestro/src/lib/api/axelarscan/hooks.ts
+++ apps/maestro/src/lib/api/axelarscan/hooks.ts
@@ -5,14 +5,19 @@ import { DISABLED_CHAINS } from "~/config/chains";
 import { getAssetPrices, getAssets, getChainConfigs } from ".";
 
 export function useChainConfigsQuery() {
-  return useQuery(["axelarscan-chain-configs"], getChainConfigs);
+  return useQuery(["axelarscan-chain-configs"], getChainConfigs, {
+    staleTime: 1000 * 60 * 60, // 1 hour
+    refetchOnWindowFocus: false,
+  });
 }
 
 export function useEVMChainConfigsQuery() {
   const { data, ...query } = useChainConfigsQuery();
 
   return {
-    data: data?.evm?.filter((a) => !DISABLED_CHAINS.has(a?.chain_id)),
+    data: data?.evm?.filter(
+      (chain) => !(DISABLED_CHAINS.has(chain?.chain_id) || chain?.deprecated)
+    ),
     ...query,
   };
 }
