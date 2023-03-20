110e5c7b26f733c31aecc8015c5ee09abd39b703
Canh Trinh <canh@axelar.network>
2023-03-20 19:22:40 -0400

    feat: adding window for screen 1 of adding erc20 token

--- apps/maestro/src/features/AddErc20/AddErc20.state.ts
+++ apps/maestro/src/features/AddErc20/AddErc20.state.ts
@@ -1,1 +1,12 @@
+import { useState } from "react";
+
 export const INITIAL_STATE = {};
+
+export const useAddErc20State = () => {
+  const [step, setStep] = useState(1);
+
+  return {
+    step,
+    setStep,
+  };
+};
