3fe4cc70f97bc2253f8b0f77a5c91c79cd548846
Canh Trinh <canh@axelar.network>
2023-03-22 15:27:11 -0400

    feat: adding status UI while waiting for register

--- apps/maestro/src/compounds/AddErc20/Steps/Step4.tsx
+++ apps/maestro/src/compounds/AddErc20/Steps/Step4.tsx
@@ -7,28 +7,50 @@ import { queryTransactionStatus } from "~/lib/api/axelarscan";
 import { StepProps } from ".";
 
 export const Step4: FC<StepProps> = (props: StepProps) => {
-  const [count, setCount] = useState(0);
   const [delay, setDelay] = useState<number | null>(10000);
 
+  const setToMap = (set: Set<string>) => {
+    const map = new Map();
+    set.forEach((k) => map.set(k.toLowerCase(), "called"));
+    return map;
+  };
+  const [statusMap, setStatusMap] = useState(setToMap(props.selectedChains));
+  const updateStatusMap = (chainId: string, status: string) => {
+    setStatusMap(new Map(statusMap.set(chainId, status)));
+  };
   const updateState = useCallback(async () => {
-    console.log("txHashhhh", count, props.txHash, props.selectedChains);
-    const response = await queryTransactionStatus(props.txHash);
-
-    if (count < 3) {
-      setCount((count) => count + 1);
-    } else {
-      setDelay(null);
-    }
-    console.log("response", response);
-    return response;
-  }, [props.txHash, count]);
+    const { data } = await queryTransactionStatus(props.txHash);
+    data.forEach((tx: any) => {
+      const { destinationChain } = tx.call.returnValues;
+      if (statusMap.get(destinationChain.toLowerCase()) !== tx.status) {
+        updateStatusMap(destinationChain.toLowerCase(), tx.status);
+      }
+    });
+    if (data.every((tx: any) => tx.status === "executed")) setDelay(null);
+    return data;
+  }, [props.txHash, updateStatusMap, statusMap]);
 
   useIntervalAsync(updateState, delay);
+
+  console.log("statusMap", statusMap);
+  const getStatuses = () => {
+    let divs = [<div>hi</div>];
+    statusMap.forEach((status: string, chainId: string) => {
+      console.log(chainId + " = " + status);
+      divs.push(
+        <div key={`chain-status-${chainId}`}>
+          Chain: {chainId}, Status: {status}
+        </div>
+      );
+    });
+    return <div>{divs}</div>;
+  };
 
   return (
     <div>
       <div>Deploy Token Successful</div>
       <LinkButton>{props.deployedTokenAddress}</LinkButton>
+      <div>{getStatuses()}</div>
     </div>
   );
 };
