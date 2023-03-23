86e36937ea420c76c8763643e9621fe55ef14e94
Alan Soares <alanrsoars@gmail.com>
2023-03-23 14:07:07 +1300

    refactor: rename util to isValidEVMAddress

--- apps/maestro/src/lib/utils/isValidERC20Address.ts
+++
@@ -1,9 +0,0 @@
-import { getAddress } from "ethers/lib/utils.js";
-
-export const isValidAddress = (address: string): address is `0x${string}` => {
-  try {
-    return typeof getAddress(address) === "string";
-  } catch (error) {
-    return false;
-  }
-};
