a197eb54c61eda78e35805ff7abace21be4922e9
Alan Soares <alanrsoars@gmail.com>
2023-04-04 08:47:05 +1200

    feat(ui): introduce alert component

---
+++ packages/ui/src/components/Alert/Alert.tsx
@@ -0,0 +1,53 @@
+import type { ComponentProps, FC, ReactNode } from "react";
+
+import { cva, VariantProps } from "class-variance-authority";
+import { AlertTriangle, CheckCircle, Info, XCircle } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+import tw from "tailwind-styled-components";
+
+const StyledAlert = tw.div``;
+
+const alertVariance = cva("alert", {
+  variants: {
+    status: {
+      info: "alert-info",
+      success: "alert-success",
+      warning: "alert-warning",
+      error: "alert-error",
+    },
+  },
+});
+
+export type AlertProps = ComponentProps<typeof StyledAlert> &
+  VariantProps<typeof alertVariance> & {
+    icon?: ReactNode;
+  };
+
+const ICON_MAP: Record<NonNullable<AlertProps["status"]>, ReactNode> = {
+  info: <Info />,
+  success: <CheckCircle />,
+  warning: <AlertTriangle />,
+  error: <XCircle />,
+};
+
+export const Alert: FC<AlertProps> = ({
+  status,
+  icon,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <StyledAlert
+      className={twMerge(alertVariance({ status }), className)}
+      {...props}
+    >
+      <div>
+        <span className={!status ? "text-info" : ""}>
+          {icon ?? ICON_MAP[status ?? "info"]}
+        </span>
+        <span>{children}</span>
+      </div>
+    </StyledAlert>
+  );
+};
