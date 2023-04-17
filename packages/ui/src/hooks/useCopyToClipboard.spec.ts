4683bd40f6c5c23bb014c0415ee5483496a16518
alanrsoares <alan@axelar.network>
2023-04-17 13:27:00 +1200

    chore(test): add unit tests for useCopyToClipboard

---
+++ packages/ui/src/hooks/useCopyToClipboard.spec.ts
@@ -0,0 +1,60 @@
+// useCopyToClipboard.test.ts
+
+import { act, renderHook } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, it, vitest } from "vitest";
+
+import { useCopyToClipboard } from "./useCopyToClipboard";
+
+describe("useCopyToClipboard", () => {
+  beforeEach(() => {
+    vitest.useFakeTimers();
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: vitest.fn(() => Promise.resolve()),
+      },
+    });
+  });
+
+  afterEach(() => {
+    vitest.useRealTimers();
+    vitest.resetAllMocks();
+  });
+
+  it("copies the given text to clipboard and updates the copiedText state", async () => {
+    const { result, rerender } = renderHook(useCopyToClipboard);
+    const [copiedText, copy] = result.current;
+
+    expect(copiedText).toBe(null);
+
+    const textToCopy = "test text";
+    const success = await act(() => copy(textToCopy));
+
+    expect(success).toBe(true);
+    const [updatedCopiedText] = result.current;
+    expect(updatedCopiedText).toBe(textToCopy);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(textToCopy);
+
+    vitest.advanceTimersByTime(3000);
+
+    rerender();
+
+    const [resetCopiedText] = result.current;
+    expect(resetCopiedText).toBe(null);
+  });
+
+  it("handles clipboard write failure", async () => {
+    vitest
+      .spyOn(navigator.clipboard, "writeText")
+      .mockRejectedValue(new Error("Copy failed"));
+
+    const { result } = renderHook(useCopyToClipboard);
+    const [, copy] = result.current;
+
+    const textToCopy = "test text";
+    const success = await act(() => copy(textToCopy));
+
+    expect(success).toBe(false);
+    const [failedCopiedText] = result.current;
+    expect(failedCopiedText).toBe(null);
+  });
+});
