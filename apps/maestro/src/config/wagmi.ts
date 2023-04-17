471dd1c161c253a7eb56241e94f62a5185a99e1f
alanrsoares <alan@axelar.network>
2023-04-17 19:53:37 +1200

    chore: use w3mProvider with wagmi

--- apps/maestro/src/config/wagmi.ts
+++ apps/maestro/src/config/wagmi.ts
@@ -1,5 +1,9 @@
 import { QueryClient } from "@tanstack/react-query";
-import { EthereumClient, w3mConnectors } from "@web3modal/ethereum";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
 import { configureChains, createClient } from "wagmi";
 import {
   arbitrum,
@@ -25,7 +29,6 @@ import {
   polygon,
   polygonMumbai,
 } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
 
 import { logger } from "~/lib/logger";
 
@@ -135,7 +138,9 @@ logger.info({
 });
 
 const { webSocketProvider, provider } = configureChains(EVM_CHAIN_CONFIGS, [
-  publicProvider(),
+  w3mProvider({
+    projectId: WALLECTCONNECT_PROJECT_ID,
+  }),
 ]);
 
 export const queryClient = new QueryClient();
