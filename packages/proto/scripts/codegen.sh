39ffb80e3736c085ad4fd0478ce70f7192f9009f
Alan Soares <alanrsoars@gmail.com>
2023-05-02 17:35:50 +1200

    chore: add line breaks

--- packages/proto/scripts/codegen.sh
+++ packages/proto/scripts/codegen.sh
@@ -1,7 +1,10 @@
 # Description: Generate typescript types from proto files
 
 npx rimraf src/generated/proto/* 
-protoc -I=proto -I=proto/third_party proto/axelar/**/**/*.proto --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=src --ts_proto_opt='esModuleInterop=true,forceLong=long,useOptionals=messages'
+protoc -I=proto -I=proto/third_party proto/axelar/**/**/*.proto\
+ --plugin=./node_modules/.bin/protoc-gen-ts_proto\
+ --ts_proto_out=src\
+ --ts_proto_opt='esModuleInterop=true,forceLong=long,useOptionals=messages'
 
 # remove descriptor.ts to avoid infinite type recursion
 npx rimraf src/google/protobuf/descriptor.ts
