5c29fb90750dfc3a4d63accfdbab9d10a00e314b
Alan Soares <alanrsoars@gmail.com>
2023-05-02 17:34:27 +1200

    chore: add descriptions

--- packages/proto/scripts/clean.sh
+++ packages/proto/scripts/clean.sh
@@ -1,1 +1,3 @@
+# Description: Clean generated typescript types from proto files
+
 npx rimraf axelar build cosmos cosmos_proto gogoproto google proto src/*
--- packages/proto/scripts/codegen.sh
+++ packages/proto/scripts/codegen.sh
@@ -1,3 +1,5 @@
+# Description: Generate typescript types from proto files
+
 npx rimraf src/generated/proto/* 
 protoc -I=proto -I=proto/third_party proto/axelar/**/**/*.proto --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=src --ts_proto_opt='esModuleInterop=true,forceLong=long,useOptionals=messages'
 
