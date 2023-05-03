747d30738248e4b12d48d8b32af4b7efd677b88f
Alan Soares <alanrsoars@gmail.com>
2023-05-03 13:30:58 +1200

    feat(packages/cosmos): migrate cosmos clients from monolith sdk

---
+++ packages/cosmos/src/signing-client/axelarnet-tx-types.ts
@@ -0,0 +1,55 @@
+import {
+  AddCosmosBasedChainRequest,
+  ConfirmDepositRequest,
+  ExecutePendingTransfersRequest,
+  LinkRequest,
+  protobufPackage,
+  RegisterAssetRequest,
+  RegisterFeeCollectorRequest,
+  RegisterIBCPathRequest,
+  RouteIBCTransfersRequest,
+} from "@axelarjs/proto/axelar/axelarnet/v1beta1/tx";
+import { Registry } from "@cosmjs/proto-signing";
+
+const TxTypeUrlMap = {
+  AxelarnetLinkRequest: `/${protobufPackage}.LinkRequest`,
+  AxelarnetConfirmDepositRequest: `/${protobufPackage}.ConfirmDepositRequest`,
+  AxelarnetExecutePendingTransfersRequest: `/${protobufPackage}.ExecutePendingTransfersRequest`,
+  AxelarnetRegisterIBCPathRequest: `/${protobufPackage}.RegisterIBCPathRequest`,
+  AxelarnetAddCosmosBasedChainRequest: `/${protobufPackage}.AddCosmosBasedChainRequest`,
+  AxelarnetRegisterAssetRequest: `/${protobufPackage}.RegisterAssetRequest`,
+  AxelarnetRouteIBCTransfersRequest: `/${protobufPackage}.RouteIBCTransfersRequest`,
+  AxelarnetRegisterFeeCollectorRequest: `/${protobufPackage}.RegisterFeeCollectorRequest`,
+};
+
+export function registerAxelarnetTxTypes(registry: Registry) {
+  registry.register(TxTypeUrlMap.AxelarnetLinkRequest, LinkRequest);
+  registry.register(
+    TxTypeUrlMap.AxelarnetConfirmDepositRequest,
+    ConfirmDepositRequest
+  );
+  registry.register(
+    TxTypeUrlMap.AxelarnetExecutePendingTransfersRequest,
+    ExecutePendingTransfersRequest
+  );
+  registry.register(
+    TxTypeUrlMap.AxelarnetRegisterIBCPathRequest,
+    RegisterIBCPathRequest
+  );
+  registry.register(
+    TxTypeUrlMap.AxelarnetAddCosmosBasedChainRequest,
+    AddCosmosBasedChainRequest
+  );
+  registry.register(
+    TxTypeUrlMap.AxelarnetRegisterAssetRequest,
+    RegisterAssetRequest
+  );
+  registry.register(
+    TxTypeUrlMap.AxelarnetRouteIBCTransfersRequest,
+    RouteIBCTransfersRequest
+  );
+  registry.register(
+    TxTypeUrlMap.AxelarnetRegisterFeeCollectorRequest,
+    RegisterFeeCollectorRequest
+  );
+}
