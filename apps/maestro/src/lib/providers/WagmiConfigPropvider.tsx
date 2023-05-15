73340832263d719c0f51ee4f87d61a2c09bebccb
Alan Soares <alanrsoars@gmail.com>
2023-05-16 10:15:01 +1200

    feat: add ledger support + upgrade wagmi

--- apps/maestro/src/lib/providers/WagmiConfigPropvider.tsx
+++ apps/maestro/src/lib/providers/WagmiConfigPropvider.tsx
@@ -5,5 +5,8 @@ import { WagmiConfig } from "wagmi";
 import { wagmiConfig } from "~/config/wagmi";
 
 export const WagmiConfigPropvider: FC<PropsWithChildren> = ({ children }) => {
+  if (!wagmiConfig) {
+    return <>{children}</>;
+  }
   return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
 };
