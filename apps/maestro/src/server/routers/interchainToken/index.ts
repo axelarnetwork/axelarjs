f99feb8e701619613f85df420d8981113fbbc636
Alan Soares <alanrsoars@gmail.com>
2023-05-22 17:33:48 +1200

    feat: create interchainToken trpc router

---
+++ apps/maestro/src/server/routers/interchainToken/index.ts
@@ -0,0 +1,12 @@
+import { router } from "~/server/trpc";
+
+import { getInterchainTokenBalanceForOwner } from "./getInterchainTokenBalanceForOwner";
+import { getInterchainTokenDetails } from "./getInterchainTokenDetails";
+
+export const interchainTokenRouter = router({
+  getInterchainTokenDetails,
+  getInterchainTokenBalanceForOwner,
+});
+
+// export type definition of API
+export type InterchainTokenRouter = typeof interchainTokenRouter;
