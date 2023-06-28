b2352de953856e683ca55d21e51f948ac1afd7a9
Alan Soares <alanrsoars@gmail.com>
2023-06-28 14:57:05 +1200

    fix: openapi baseUrl

--- apps/maestro/src/server/openapi.ts
+++ apps/maestro/src/server/openapi.ts
@@ -1,3 +1,5 @@
+import { Maybe } from "@axelarjs/utils";
+
 import { generateOpenApiDocument } from "trpc-openapi";
 
 import { appRouter } from "./routers/_app";
@@ -7,9 +9,10 @@ export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "Intercahin Maestro API",
   description: "Interchain Maestro API, search interchain tokens",
   version: "1.0.0",
-  baseUrl: `${
-    process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000"
-  }/api`,
+  baseUrl: `${Maybe.of(process.env.NEXT_PUBLIC_VERCEL_URL).mapOr(
+    "http://localhost:3000",
+    (url) => `https://${url}`
+  )}/api`,
   docsUrl: "https://github.com/axelarnetwork/axelarjs/tree/main/apps/maestro",
   tags: ["interchain-token"],
 });
