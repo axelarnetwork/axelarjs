0bd10cbadb8fa021f057a76d13b8443d5208ef3c
Alan Soares <alanrsoars@gmail.com>
2023-07-13 19:08:53 +1200

    chore: simplify session management

--- apps/maestro/src/server/routers/auth/index.ts
+++ apps/maestro/src/server/routers/auth/index.ts
@@ -3,11 +3,9 @@ import { signin } from "./signin";
 
 export const authRouter = router({
   signin,
-  getSession: publicProcedure.query(async ({ ctx }) => {
-    return {
-      address: ctx.session?.address,
-    };
-  }),
+  getSession: publicProcedure.query(({ ctx }) =>
+    ctx.session ? { address: ctx.session?.address } : null
+  ),
 });
 
 export type AuthRouter = typeof authRouter;
