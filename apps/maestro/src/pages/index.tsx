fcb03916f84be04829c691cb8d6f3cde68fd0cf9
Canh Trinh <canh@axelar.network>
2023-07-17 14:23:44 -0400

    chore: update testnet banner text and add persisted state

--- apps/maestro/src/pages/index.tsx
+++ apps/maestro/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Button, Card } from "@axelarjs/ui";
 import { sluggify } from "@axelarjs/utils";
+import { useLocalStorageState } from "@axelarjs/utils/react";
 import { useCallback } from "react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 
 import { useNetwork } from "wagmi";
 
+import { NEXT_PUBLIC_NETWORK_ENV } from "~/config/env";
 import SearchInterchainToken from "~/features/SearchInterchainToken";
 import Page from "~/layouts/Page";
 
@@ -14,6 +16,12 @@ const AddErc20 = dynamic(() => import("~/features/AddErc20/AddErc20"));
 export default function Home() {
   const router = useRouter();
   const { chain } = useNetwork();
+  const [persistedState, setPersistedState] = useLocalStorageState(
+    "@axelar/maestro/showBetaBanner",
+    {
+      showBetaBanner: true,
+    }
+  );
 
   const handleTokenFound = useCallback(
     (result: { tokenAddress: string; tokenId?: string }) => {
@@ -33,14 +41,22 @@ export default function Home() {
       mustBeConnected
     >
       <div className="flex w-full max-w-lg flex-col items-center justify-center">
-        <Card compact className="absolute top-20 w-1/2 bg-slate-200 p-5">
-          <Button shape="square" size="sm">
-            x
-          </Button>
-          <Card.Body>
-            You are using Maestro Beta in testnet. Further improvements to come.
-          </Card.Body>
-        </Card>
+        {NEXT_PUBLIC_NETWORK_ENV === "testnet" &&
+          persistedState.showBetaBanner && (
+            <Card compact className="absolute top-20 w-1/3 bg-slate-200 p-5">
+              <Button
+                shape="square"
+                size="sm"
+                onClick={() => setPersistedState({ showBetaBanner: false })}
+              >
+                x
+              </Button>
+              <Card.Body>
+                You are using Maestro Beta in testnet. The app is in active
+                development; further updates and improvements to come!
+              </Card.Body>
+            </Card>
+          )}
         <SearchInterchainToken onTokenFound={handleTokenFound} />
         <div className="divider">OR</div>
         <AddErc20 />
