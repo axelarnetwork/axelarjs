5a8caf95d87678bf81306186b38e0a3b6da8c0a0
Alan Soares <alanrsoars@gmail.com>
2023-07-20 17:14:50 +1200

    refactor: migrate script env to ts

--- apps/maestro/scripts/env.ts
+++ apps/maestro/scripts/env.ts
@@ -1,26 +1,26 @@
-const fs = require("fs").promises;
-const dotenv = require("dotenv");
-const prettier = require("prettier");
+#!/usr/bin/env zx
+import fs from "fs/promises";
+import dotenv from "dotenv";
+import prettier from "prettier";
 
 /**
  * Generates a TypeScript export statement for an environment variable.
  *
  * @param {string} envVar - The name of the environment variable.
- * @returns {string} The TypeScript export statement.
  */
-const generateExportStatement = (envVar) => `
-export const ${envVar} = Maybe.of<string>(process.env.${envVar});
+const generateExportStatement = (envVar: string) => `
+  export const ${envVar} = Maybe.of<string>(process.env.${envVar});
 `;
 
 /**
  * Generates a TypeScript file with export statements for the given environment variables.
  *
  * @param {string[]} envVars - The names of the environment variables.
- * @returns {string} The TypeScript file content.
  */
-const generateTsFileContent = (envVars) => `
-import { Maybe } from "@axelarjs/utils";
-${envVars.map(generateExportStatement).join("\n")}
+const generateTsFileContent = (envVars: string[]) => `
+  import { Maybe } from "@axelarjs/utils";
+
+  ${envVars.map(generateExportStatement).join("\n")}
 `;
 
 /**
@@ -29,7 +29,7 @@ ${envVars.map(generateExportStatement).join("\n")}
  * @param {string} filePath - The path of the file.
  * @param {string} content - The content to write.
  */
-const writeToFile = async (filePath, content) => {
+async function writeToFile(filePath: string, content: string) {
   try {
     await fs.writeFile(filePath, content);
     console.log("Generated TypeScript file with environment variables.");
@@ -39,31 +39,30 @@ const writeToFile = async (filePath, content) => {
       err
     );
   }
-};
+}
 
 /**
  * Tries to read a .env file and generate a TypeScript file with type-safe environment variables.
  *
  * @param {string} envFilePath - The path of the .env file.
  * @param {string} outputPath - The path of the TypeScript file to generate.
- * @returns {Promise<boolean>} A promise that resolves to true if the .env file was successfully read and the TypeScript file was generated, or false otherwise.
  */
-const tryGenerateTsFile = async (envFilePath, outputPath) => {
+async function tryGenerateTsFile(envFilePath: string, outputPath: string) {
   try {
     const data = await fs.readFile(envFilePath);
     const envConfig = dotenv.parse(data);
     const envVars = Object.keys(envConfig);
-    const fileContent = generateTsFileContent(envVars);
-    const formattedFileContent = prettier.format(fileContent, {
+    const content = generateTsFileContent(envVars);
+    const formatted = prettier.format(content, {
       parser: "babel-ts",
     });
 
-    await writeToFile(outputPath, formattedFileContent);
+    await writeToFile(outputPath, formatted);
     return true;
   } catch (err) {
     return false;
   }
-};
+}
 
 /**
  * Generates a TypeScript file with type-safe environment variables read from a list of .env files.
@@ -71,13 +70,13 @@ const tryGenerateTsFile = async (envFilePath, outputPath) => {
  * @param {string[]} envFilePaths - The paths of the .env files.
  * @param {string} outputPath - The path of the TypeScript file to generate.
  */
-const generateTsFile = async (envFilePaths, outputPath) => {
+async function generateTsFile(envFilePaths: string[], outputPath: string) {
   for (const envFilePath of envFilePaths) {
     const success = await tryGenerateTsFile(envFilePath, outputPath);
     if (success) {
       break;
     }
   }
-};
+}
 
 generateTsFile([".env.local", ".env"], "src/config/env-safe.ts");
