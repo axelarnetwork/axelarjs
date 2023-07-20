dfbc2c9f5c43bb244f7036f39b1e55a8deacc16f
Alan Soares <alanrsoars@gmail.com>
2023-07-20 14:43:03 +1200

    refactor: migrate packages/evm scripts to ts

--- packages/evm/scripts/build.sh
+++ packages/evm/scripts/build.sh
@@ -1,3 +1,6 @@
-pnpm tsc -m commonjs --outDir build/commonjs 
-pnpm tsc -m esnext --outDir build/module 
+
+# build only files under src
+pnpm tsc -p tsconfig.build.json -m commonjs --outDir build/commonjs 
+pnpm tsc -p tsconfig.build.json -m esnext --outDir build/module
+
 cp -Rf build/commonjs/* .
