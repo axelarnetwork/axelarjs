6803ba24c9f53b89e31b4b4db24d7a760a5a3286
Alan Soares <alanrsoars@gmail.com>
2023-08-07 16:44:09 +1200

    chore: extract inline connection string

--- apps/maestro/drizzle.config.ts
+++ apps/maestro/drizzle.config.ts
@@ -3,11 +3,13 @@ import type { Config } from "drizzle-kit";
 
 dotenv.config({ path: ".env.local" });
 
+const CONNECTION_STRING = `${process.env.POSTGRES_URL}?sslmode=require`;
+
 export default {
   schema: "./src/lib/drizzle/schema",
   out: "./src/lib/drizzle/client",
   driver: "pg",
   dbCredentials: {
-    connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
+    connectionString: CONNECTION_STRING,
   },
 } satisfies Config;
