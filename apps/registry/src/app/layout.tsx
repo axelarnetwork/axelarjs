526af461ce7036b01aa3506474e06cc6a1eb9a8f
Alan Soares <alanrsoars@gmail.com>
2023-08-24 17:50:47 +1200

    chore: normalize chain card content

--- apps/registry/src/app/layout.tsx
+++ apps/registry/src/app/layout.tsx
@@ -1,15 +1,27 @@
-"use client";
-
 import { cn } from "@axelarjs/ui/utils";
 
 import "./globals.css";
 
 import { PropsWithChildren } from "react";
+import { Metadata } from "next";
 import { Cabin } from "next/font/google";
 
 import MainLayout from "~/layouts/MainLayout";
 
 const font = Cabin({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.png",
+    },
+    {
+      rel: "shortcut icon",
+      url: "/favicon.png",
+    },
+  ],
+};
 
 type Props = PropsWithChildren;
 
