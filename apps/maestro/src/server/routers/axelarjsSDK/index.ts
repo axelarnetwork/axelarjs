23f7c8f3d617b962db5d5b2f5df38e08a63aefba
Alan Soares <alanrsoars@gmail.com>
2023-08-30 11:44:40 +1200

    feat: add useChainInfoQuery + derive hook input type from trpc procedure

--- apps/maestro/src/server/routers/axelarjsSDK/index.ts
+++ apps/maestro/src/server/routers/axelarjsSDK/index.ts
@@ -3,6 +3,10 @@ import { estimateGasFee } from "./estimateGasFee";
 import { estimateGasFeesMultipleChains } from "./estimateGasFeeMultipleChains";
 import { getChainInfo } from "./getChainInfo";
 
+export type * from "./estimateGasFee";
+export type * from "./estimateGasFeeMultipleChains";
+export type * from "./getChainInfo";
+
 export const axelarjsSDKRouter = router({
   estimateGasFee,
   estimateGasFeesMultipleChains,
