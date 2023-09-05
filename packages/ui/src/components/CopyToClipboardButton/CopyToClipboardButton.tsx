17ccfb903f4ebef9d098c34615a4384936891711
Alan Soares <alanrsoars@gmail.com>
2023-09-05 14:47:00 +1200

    fix: missing prop reference

--- packages/ui/src/components/CopyToClipboardButton/CopyToClipboardButton.tsx
+++ packages/ui/src/components/CopyToClipboardButton/CopyToClipboardButton.tsx
@@ -51,7 +51,9 @@ export const CopyToClipboardButton = forwardRef<
         <CheckCircleIcon
           className={cn(
             "swap-on h-[1em] w-[1em]",
-            props.color === "success" ? "text-success-content" : "text-success"
+            props.variant === "success"
+              ? "text-success-content"
+              : "text-success"
           )}
         />
         <CopyIcon className={cn("swap-off", "h-[1em] w-[1em]")} />
