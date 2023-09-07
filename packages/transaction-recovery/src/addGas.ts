bc61c145cade0cd7d6a24e91c9025b4de0958a6f
Alan Soares <alanrsoars@gmail.com>
2023-09-07 14:19:12 +1200

    chore: fix tests

--- packages/transaction-recovery/src/addGas.ts
+++ packages/transaction-recovery/src/addGas.ts
@@ -1,8 +1,20 @@
+import { createGMPNodeClient } from "@axelarjs/api/gmp/node";
 import type { AxelarSigningClient } from "@axelarjs/cosmos/signing-client";
+
+const gmpClient = createGMPNodeClient({
+  prefixUrl: "https://testnet.api.gmp.axelarscan.io",
+});
 
 async function addGas(signingClient: AxelarSigningClient, txHash: string) {
   const tx = await signingClient.getTx(txHash);
   console.log({ tx });
+
+  const fees = await gmpClient.getFees({
+    destinationChain: "ethereum-2",
+    sourceChain: "polygon",
+  });
+
+  console.log({ fees });
 
   // send ibc transfer with memo field using message ID from tx above
   // todo
