2f740319001e669f42715b1225f4cb40596eebae
Canh Trinh <canh@axelar.network>
2023-09-11 21:24:11 -0400

    chore: wip with s3 client integration

--- packages/transaction-recovery/src/services/index.ts
+++ packages/transaction-recovery/src/services/index.ts
@@ -1,7 +1,13 @@
 import { createGMPNodeClient } from "@axelarjs/api/gmp/node";
+import { createS3NodeClient } from "@axelarjs/api/s3/node";
 import { AXELARSCAN_API_URLS, Environment } from "@axelarjs/core";
 
 export const gmpClient = (env: Environment) =>
   createGMPNodeClient({
     prefixUrl: AXELARSCAN_API_URLS[env],
+  });
+
+export const s3Client = (env: Environment) =>
+  createS3NodeClient({
+    prefixUrl: "",
   });
