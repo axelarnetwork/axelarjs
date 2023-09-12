aa7edc42f4de0e12bc38738d7a477e78270936eb
Canh Trinh <canh@axelar.network>
2023-09-12 18:50:45 -0400

    chore: adding signAndGetTxBytes

--- packages/cosmos/src/client.spec.ts
+++ packages/cosmos/src/client.spec.ts
@@ -1,10 +1,31 @@
 import { MsgServiceClientImpl } from "@axelarjs/proto/axelar/axelarnet/v1beta1/service";
 
+import { DirectSecp256k1HdWallet, EncodeObject } from "@cosmjs/proto-signing";
+import { GasPrice, SigningStargateClient, StdFee } from "@cosmjs/stargate";
+import { toAccAddress } from "@cosmjs/stargate/build/queryclient/utils";
+import { HttpClient, Tendermint37Client } from "@cosmjs/tendermint-rpc";
 import grpc, { Client } from "@grpc/grpc-js";
+import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 import { Rpc } from "cosmjs-types/helpers";
 
 import { createAxelarQueryClient } from "./rpc-client";
+import { createAxelarSigningClient } from "./signing-client";
 
+export const STANDARD_FEE: StdFee = {
+  amount: [
+    {
+      denom: "uaxl",
+      amount: "1000",
+    },
+  ],
+  gas: "5000000",
+};
+
+export const defaultSigningClientOptions = {
+  broadcastPollIntervalMs: 300,
+  broadcastTimeoutMs: 8_000,
+  gasPrice: GasPrice.fromString("0.01ucosm"),
+};
 class RpcIml implements Rpc {
   protected axelarRpcUrl: string;
   constructor(axelarRpcUrl: string) {
@@ -15,50 +36,109 @@ class RpcIml implements Rpc {
     service: string,
     method: string,
     data: Uint8Array
-  ): Promise<Uint8Array> {
-    const rpcClient = new Client(
+  ): Promise<any> {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+      process.env["COSMOS_WALLET_MNEMONIC"] as string,
+      { prefix: "axelar" }
+    );
+    const client = await SigningStargateClient.connectWithSigner(
       this.axelarRpcUrl,
-      grpc.credentials.createInsecure()
+      wallet,
+      defaultSigningClientOptions
     );
+    const payload: EncodeObject[] = [
+      {
+        typeUrl: `axelar.axelarnet.v1beta1.LinkRequest`,
+        value: data,
+      },
+    ];
 
-    const deadline = Date.now() + 100;
-    await rpcClient.waitForReady(deadline, () => {});
+    const signerAddress = await wallet
+      .getAccounts()
+      .then((acc) => acc[0]?.address);
 
-    const call = rpcClient.makeUnaryRequest(
-      `${service}/${method}`,
-      (arg) => arg,
-      (arg) => arg,
-      Buffer.from(data),
-      () => {}
+    const signAndGetTxBytes = async (
+      signerAddress: string,
+      messages: EncodeObject[],
+      fee: StdFee,
+      memo: string
+    ) => {
+      const txRaw = await client.sign(signerAddress, messages, fee, memo);
+      return TxRaw.encode(txRaw).finish();
+    };
+
+    return signAndGetTxBytes(
+      signerAddress as string,
+      payload,
+      STANDARD_FEE,
+      "hello CTT"
     );
-
-    call.on("data", (data) => {
-      console.log({ data });
-    });
-
-    call.on("end", (e) => console.log("ended"));
   }
 }
+
+// class RpcIml implements Rpc {
+//   protected axelarRpcUrl: string;
+//   constructor(axelarRpcUrl: string) {
+//     this.axelarRpcUrl = axelarRpcUrl;
+//   }
+
+//   public async request(
+//     service: string,
+//     method: string,
+//     data: Uint8Array
+//   ): Promise<Uint8Array> {
+//     const rpcClient = new Client(
+//       this.axelarRpcUrl,
+//       grpc.credentials.createInsecure()
+//     );
+
+//     const deadline = Date.now() + 100;
+//     await rpcClient.waitForReady(deadline, () => {});
+
+//     const call = rpcClient.makeUnaryRequest(
+//       `${service}/${method}`,
+//       (arg) => arg,
+//       (arg) => arg,
+//       Buffer.from(data),
+//       () => {}
+//     );
+
+//     call.on("data", (data) => {
+//       console.log({ data });
+//     });
+
+//     call.on("end", (e) => console.log("ended"));
+//   }
+// }
+
 describe("query client", () => {
   test("do stuff", async () => {
     const axelarRpcUrl = "https://rpc-axelar-testnet.imperator.co:443";
-    const queryApi = await createAxelarQueryClient({
-      environment: "testnet",
-      axelarRpcUrl,
-    });
+    // const queryApi = await createAxelarQueryClient({
+    //   environment: "testnet",
+    //   axelarRpcUrl,
+    // });
 
-    const queryApiRes = await queryApi.query.evm.Chains({ status: 1 });
+    // const queryApiRes = await queryApi.query.evm.Chains({ status: 1 });
 
     const broadcastApi = new MsgServiceClientImpl(new RpcIml(axelarRpcUrl));
 
+    // const signingStargateClientAXL = createAxelarSigningClient({
+    //   cosmosBasedWalletDetails: {
+    //     mnemonic: "",
+    //   },
+    //   environment: "testnet",
+    //   options: {},
+    // });
+
     const broadcastRes = await broadcastApi.Link({
-      sender: new Uint8Array(),
-      recipientAddr: "",
-      recipientChain: "",
-      asset: "",
+      sender: toAccAddress("axelar1x3z2vepjd7fhe30epncxjrk0lehq7xdq4j6ndq"),
+      recipientAddr: "0xB8Cd93C83A974649D76B1c19f311f639e62272BC",
+      recipientChain: "avalanche",
+      asset: "uaxl",
     });
 
     expect(broadcastRes).toBeTruthy();
-    expect(queryApiRes).toBeTruthy();
+    // expect(queryApiRes).toBeTruthy();
   });
 });
