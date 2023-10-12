652d7d3c85b52bf91bec767d1ce42dec27c31f60
Alan Soares <alanrsoars@gmail.com>
2023-10-13 10:29:14 +1300

    chore: add explicyt @sentry/cli dependency

--- apps/maestro/next.config.js
+++ apps/maestro/next.config.js
@@ -34,9 +34,6 @@ export default withSentryConfig(
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: "/monitoring",
 
