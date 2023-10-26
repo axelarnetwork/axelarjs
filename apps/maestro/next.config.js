864b57c8ceca7f7c8d3b161ebfded7520474372d
Alan Soares <alanrsoars@gmail.com>
2023-10-27 11:05:48 +1300

    chore: revert maestro to nextjs 13.x

--- apps/maestro/next.config.js
+++ apps/maestro/next.config.js
@@ -9,9 +9,6 @@ const nextConfig = {
   images: {
     domains: ["testnet.axelarscan.io"],
   },
-  experimental: {
-    webpackBuildWorker: true,
-  },
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
