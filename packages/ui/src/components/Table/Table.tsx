f7bd39e01ae40b20204d085eb1b56eed063fba03
Alan Soares <alanrsoars@gmail.com>
2023-11-02 14:03:50 +1300

    feat: add Table component

---
+++ packages/ui/src/components/Table/Table.tsx
@@ -0,0 +1,61 @@
+import { ComponentProps } from "react";
+
+import { cva, VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+import tw from "../../tw";
+
+const tableVariance = cva("table", {
+  variants: {
+    zebra: {
+      true: "table-zebra",
+    },
+    pinRows: {
+      true: "table-pin-rows",
+    },
+    pinCols: {
+      true: "table-pin-cols",
+    },
+    size: {
+      xs: "table-xs",
+      sm: "table-sm",
+      md: "table-md",
+      lg: "table-lg",
+    },
+  },
+});
+
+const StyledTable = tw.div``;
+
+type TableBaseProps = VariantProps<typeof tableVariance>;
+
+export interface TableProps
+  extends ComponentProps<typeof StyledTable>,
+    TableBaseProps {}
+
+const TableContainer = ({
+  zebra,
+  pinRows,
+  pinCols,
+  size,
+  className,
+  ...props
+}: TableProps) => {
+  return (
+    <StyledTable
+      className={twMerge(
+        tableVariance({ zebra, pinRows, pinCols, size }),
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const Table = Object.assign(TableContainer, {
+  Head: tw.thead``,
+  Column: tw.th``,
+  Body: tw.tbody``,
+  Row: tw.tr``,
+  Cell: tw.td``,
+});
