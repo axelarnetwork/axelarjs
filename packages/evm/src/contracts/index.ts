037f8fffb83b2169b8de8acd5b591e5ae35ccac4
Alan Soares <alanrsoars@gmail.com>
2023-11-13 15:04:29 +1300

    chore: export InterchainToken client

--- packages/evm/src/contracts/index.ts
+++ packages/evm/src/contracts/index.ts
@@ -1,5 +1,6 @@
 export * from "./its/ERC20";
 export * from "./its/IERC20MintableBurnable";
+export * from "./its/InterchainToken";
 export * from "./its/InterchainTokenService";
 export * from "./its/InterchainTokenFactory";
 export * from "./its/TokenManager";
