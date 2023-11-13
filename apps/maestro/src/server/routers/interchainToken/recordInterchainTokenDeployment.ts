c484cbf9a7bf2b8931bf4615267148a73062f321
Alan Soares <alanrsoars@gmail.com>
2023-11-14 12:25:29 +1300

    refactor: migrate recordInterchainTokenDeployment persistence layer

--- apps/maestro/src/server/routers/interchainToken/recordInterchainTokenDeployment.ts
+++ apps/maestro/src/server/routers/interchainToken/recordInterchainTokenDeployment.ts
@@ -14,34 +14,21 @@ export const recordInterchainTokenDeployment = protectedProcedure
   .mutation(async ({ ctx, input }) => {
     invariant(ctx.session?.address, "ctx.session.address is required");
 
-    // await ctx.persistence.postgres.recordInterchainTokenDeployment({
-    //   deployerAddress: ctx.session.address,
-    //   ...input,
-    // });
+    await ctx.persistence.postgres.recordInterchainTokenDeployment({
+      deployerAddress: ctx.session.address,
+      ...input,
+    });
 
-    // await ctx.persistence.postgres.recordRemoteInterchainTokenDeployments(
-    //   input.remoteTokens.map((remoteToken) => ({
-    //     tokenId: input.tokenId,
-    //     originTokenId: input.tokenId,
-    //     address: input.tokenAddress,
-    //     deploymentTxHash: remoteToken.deploymentTxHash,
-    //     deploymentStatus: remoteToken.deploymentStatus,
-    //     chainId: remoteToken.chainId,
-    //     axelarChainId: remoteToken.axelarChainId,
-    //     deploymentLogIndex: remoteToken.deploymentLogIndex,
-    //   }))
-    // );
-
-    await ctx.persistence.kv
-      .recordInterchainTokenDeployment(
-        {
-          chainId: input.chainId,
-          tokenAddress: input.tokenAddress,
-        },
-        {
-          ...input,
-          deployerAddress: ctx.session.address,
-        }
-      )
-      .catch((e) => console.log("error recordInterchainTokenDeployment", e));
+    await ctx.persistence.postgres.recordRemoteInterchainTokenDeployments(
+      input.remoteTokens.map((remoteToken) => ({
+        tokenId: input.tokenId,
+        originTokenId: input.tokenId,
+        address: input.tokenAddress,
+        deploymentTxHash: remoteToken.deploymentTxHash,
+        deploymentStatus: remoteToken.deploymentStatus,
+        chainId: remoteToken.chainId,
+        axelarChainId: remoteToken.axelarChainId,
+        deploymentLogIndex: remoteToken.deploymentLogIndex,
+      }))
+    );
   });
