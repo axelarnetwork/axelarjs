4d43817c4929d055a8eb8fba7cd42dbb55605b13
Alan Soares <alanrsoars@gmail.com>
2023-11-16 15:43:45 +1300

    chore: add type to evm chain configs

--- apps/maestro/src/config/evm-chains.ts
+++ apps/maestro/src/config/evm-chains.ts
@@ -1,3 +1,4 @@
+import { Chain } from "viem/chains";
 import {
   arbitrum,
   arbitrumGoerli,
@@ -31,60 +32,71 @@ import {
 
 import { NEXT_PUBLIC_NETWORK_ENV } from "./env";
 
-export const EVM_CHAIN_CONFIGS = [
+interface ExtendedEVMChainConfig extends Chain {
+  axelarChainId: string;
+  axelarChainName: string;
+  environment: "mainnet" | "testnet";
+}
+
+const ENVIRONMENTS = {
+  mainnet: "mainnet",
+  testnet: "testnet",
+} as const;
+
+export const EVM_CHAIN_CONFIGS: ExtendedEVMChainConfig[] = [
   {
     ...mainnet,
     axelarChainId: "ethereum",
     axelarChainName: "ethereum",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...goerli,
     axelarChainId: "ethereum-2",
     axelarChainName: "ethereum-2",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...moonbeam,
     axelarChainId: "moonbeam",
     axelarChainName: "Moonbeam",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...moonbaseAlpha,
     axelarChainId: "moonbeam",
     axelarChainName: "Moonbeam",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...fantom,
     axelarChainId: "fantom",
     axelarChainName: "Fantom",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...fantomTestnet,
     axelarChainId: "fantom",
     axelarChainName: "Fantom",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...avalanche,
     axelarChainId: "avalanche",
     axelarChainName: "Avalanche",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...avalancheFuji,
     axelarChainId: "avalanche",
     axelarChainName: "Avalanche",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...polygon,
     axelarChainId: "polygon",
     axelarChainName: "Polygon",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...polygonMumbai,
@@ -94,110 +106,114 @@ export const EVM_CHAIN_CONFIGS = [
     },
     axelarChainId: "polygon",
     axelarChainName: "Polygon",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...polygonZkEvm,
     axelarChainId: "polygon-zkevm",
     axelarChainName: "polygon-zkevm",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...polygonZkEvmTestnet,
     axelarChainId: "polygon-zkevm",
     axelarChainName: "polygon-zkevm",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...bsc,
     axelarChainId: "binance",
     axelarChainName: "binance",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...bscTestnet,
     axelarChainId: "binance",
     axelarChainName: "binance",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...arbitrum,
     axelarChainId: "arbitrum",
     axelarChainName: "arbitrum",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...arbitrumGoerli,
     axelarChainId: "arbitrum",
     axelarChainName: "arbitrum",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...celo,
     axelarChainId: "celo",
     axelarChainName: "celo",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...celoAlfajores,
     axelarChainId: "celo",
     axelarChainName: "celo",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...aurora,
     axelarChainId: "aurora",
     axelarChainName: "aurora",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...auroraTestnet,
     axelarChainId: "aurora",
     axelarChainName: "aurora",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...optimism,
     axelarChainId: "optimism",
     axelarChainName: "optimism",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...optimismGoerli,
     axelarChainId: "optimism",
     axelarChainName: "optimism",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...kava,
     axelarChainId: "kava",
     axelarChainName: "kava",
-    environment: "mainnet",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...kavaTestnet,
     axelarChainId: "kava",
     axelarChainName: "kava",
-    environment: "testnet",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...filecoinCalibration,
     axelarChainId: "filecoin-2",
-    environment: "testnet",
+    axelarChainName: "filecoin-2",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...filecoin,
     axelarChainId: "filecoin",
-    environment: "mainnet",
+    axelarChainName: "filecoin",
+    environment: ENVIRONMENTS.mainnet,
   },
   {
     ...baseGoerli,
     axelarChainId: "base",
-    environment: "testnet",
+    axelarChainName: "base",
+    environment: ENVIRONMENTS.testnet,
   },
   {
     ...lineaTestnet,
     axelarChainId: "linea",
-    environment: "testnet",
+    axelarChainName: "linea",
+    environment: ENVIRONMENTS.testnet,
   },
 ].filter((chain) => chain.environment === NEXT_PUBLIC_NETWORK_ENV);
