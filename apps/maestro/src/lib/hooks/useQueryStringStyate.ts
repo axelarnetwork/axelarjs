ab8979f5bd48d860dae8e04ad9838163e3b19e06
alanrsoares <alan@axelar.network>
2023-12-08 11:20:05 -0300

    refactor: use next's useSearchParams instead of router

--- apps/maestro/src/lib/hooks/useQueryStringStyate.ts
+++ apps/maestro/src/lib/hooks/useQueryStringStyate.ts
@@ -1,15 +1,15 @@
 import { Maybe } from "@axelarjs/utils";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
 
 export default function useQueryStringState<TState extends string>(
   key: string,
   initialState: TState | (() => TState) = "" as TState
 ) {
-  const router = useRouter();
+  const searchParams = useSearchParams();
 
   const [state, setState] = useState<TState>(() =>
-    Maybe.of(router.query[key]).mapOr(
+    Maybe.of(searchParams.get(key)).mapOr(
       typeof initialState === "function" ? initialState() : initialState,
       (value) => value as TState
     )
@@ -19,7 +19,7 @@ export default function useQueryStringState<TState extends string>(
     const url = new URL(window.location.href);
     url.searchParams.set(key, state);
     window.history.pushState({}, "", url);
-  }, [state, key, router.query]);
+  }, [state, key, searchParams]);
 
   return [state, setState] as const;
 }
