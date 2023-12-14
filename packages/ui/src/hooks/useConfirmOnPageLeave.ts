a97aa92f6d6402f8a90325f40b11d73e53d73eee
alanrsoares <alan@axelar.network>
2023-12-14 20:35:11 -0300

    feat: prevent user from losing window context while transaction is running

---
+++ packages/ui/src/hooks/useConfirmOnPageLeave.ts
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+
+interface UseConfirmOnPageLeaveOptions {
+  enabled?: boolean;
+}
+
+/**
+ * Prompt the user with a confirmation dialog when attempting to leave the page.
+ *
+ * @param message - The message to display in the confirmation dialog.
+ * @param options - Optional configuration options.
+ * @param options.enabled - Flag to enable or disable the confirmation prompt.
+ */
+export function useConfirmOnPageLeave(
+  message: string,
+  options: UseConfirmOnPageLeaveOptions = { enabled: true }
+) {
+  useEffect(() => {
+    if (!options.enabled) {
+      return;
+    }
+
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      e.returnValue = message;
+      return message;
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [message, options.enabled]);
+}
