8e61069bb7a3755962558f44abaef25139551464
Alan Soares <alanrsoars@gmail.com>
2024-01-25 09:30:53 +1300

    fix: override NATIVE_TOKEN evaluation for ethereum-sepolia

--- apps/maestro/src/lib/utils/getNativeToken.ts
+++ apps/maestro/src/lib/utils/getNativeToken.ts
@@ -2,12 +2,22 @@ import { memoize } from "@axelarjs/utils";
 
 import { WAGMI_CHAIN_CONFIGS } from "~/config/evm-chains";
 
+export const NATIVE_TOKEN_OVERRIDE: Record<string, string> = {
+  // ethereum-sepolia actual symbol is SEP under the wagmi chain config
+  "ethereum-sepolia": "ETH",
+};
+
 /**
  * Returns the native token symbol for a given axelar chain identifier
  *
  * @param axelarChainId axerlar internal chain identifier
  */
 export const getNativeToken = memoize((axelarChainId: string) => {
+  // If we have an override, we use that
+  if (axelarChainId in NATIVE_TOKEN_OVERRIDE) {
+    return NATIVE_TOKEN_OVERRIDE[axelarChainId];
+  }
+
   const chainConfig = WAGMI_CHAIN_CONFIGS.find(
     (chain) =>
       chain.axelarChainId?.toLowerCase() === axelarChainId?.toLowerCase()
