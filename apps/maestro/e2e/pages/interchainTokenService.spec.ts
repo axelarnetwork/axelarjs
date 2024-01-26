bb3b0b2ded050a749e762c2263924f27c5cd5d01
Alan Soares <alanrsoars@gmail.com>
2024-01-26 15:06:54 +1300

    chore: fix e2e test

--- apps/maestro/e2e/pages/interchainTokenService.spec.ts
+++ apps/maestro/e2e/pages/interchainTokenService.spec.ts
@@ -1,10 +1,10 @@
 import { expect, test } from "@playwright/test";
 
-test.describe("InterchainTokenService", async () => {
+test.describe("InterchainTokenService", () => {
   test("Deploy a new interchain token", async ({ page }) => {
     await page.goto("/");
 
-    const deployButton = page.locator("button", { hasText: /Deploy/i }).nth(0);
+    const deployButton = page.locator("button", { hasText: /Connect/i }).nth(0);
 
     await expect(deployButton).toBeEnabled();
   });
