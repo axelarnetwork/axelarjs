d215feb0da97d00f4adb844ee293fc9bc653c2d0
Alan Soares <alanrsoars@gmail.com>
2024-01-31 08:00:44 +1300

    chore: scaffold competition page

---
+++ apps/maestro/src/pages/competition/index.tsx
@@ -0,0 +1,14 @@
+import Page from "~/ui/layouts/Page";
+
+const CompetitionPage = () => {
+  return (
+    <Page title="Competition" className="flex flex-col space-y-4">
+      <Page.Title>Competition</Page.Title>
+      <div className="flex-1 space-y-4">
+        <p>Competition Page</p>
+      </div>
+    </Page>
+  );
+};
+
+export default CompetitionPage;
