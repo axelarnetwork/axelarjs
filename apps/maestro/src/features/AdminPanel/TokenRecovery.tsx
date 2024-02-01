c067eaae22ef5803b2f1f0cc30cafa7abf47fde3
Alan Soares <alanrsoars@gmail.com>
2024-02-02 08:56:45 +1300

    feat: add loading state

--- apps/maestro/src/features/AdminPanel/TokenRecovery.tsx
+++ apps/maestro/src/features/AdminPanel/TokenRecovery.tsx
@@ -8,7 +8,7 @@ import {
   TextInput,
 } from "@axelarjs/ui";
 import { maskAddress, Maybe } from "@axelarjs/utils";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 
 import { trpc } from "~/lib/trpc";
 import { hex64 } from "~/lib/utils/validation";
@@ -28,86 +28,104 @@ export const TokenRecovery = () => {
   const [searchBy, setSearchBy] = useState<SearchByOption>("token_address");
   const [searchValue, setSearchValue] = useState("");
 
-  const { data } = trpc.interchainToken.findInterchainTokenByTokenId.useQuery(
-    {
-      tokenId: searchValue,
-    },
-    {
-      enabled:
-        searchBy === "token_id" && hex64().safeParse(searchValue).success,
-    }
-  );
+  const isEnabled =
+    searchBy === "token_id" && hex64().safeParse(searchValue).success;
+
+  const { data, isLoading } =
+    trpc.interchainToken.findInterchainTokenByTokenId.useQuery(
+      {
+        tokenId: searchValue,
+      },
+      {
+        enabled: isEnabled,
+      }
+    );
 
   const columns = Maybe.of(data)
     .mapOr([], (data) => Object.keys(data[0]))
-    .filter((column) => !EXCLUDED_COLUMNS.includes(column));
+    .filter((column) => !EXCLUDED_COLUMNS.includes(column))
+    .sort((a, b) => a.localeCompare(b));
 
-  const resultSection =
-    data && data.length > 0 ? (
-      <div className="grid">
-        <Card>
-          <Card.Body>
-            <Card.Title>Token Info</Card.Title>
-            <ul>
-              {Object.entries(data[0])
-                .filter(
-                  ([key, value]) =>
-                    (EXCLUDED_COLUMNS.includes(key) &&
-                      typeof value === "string") ||
-                    typeof value === "number"
-                )
-                .map(([key, value]) => (
-                  <li key={key}>
-                    <span className="mr-1 font-semibold opacity-85">
-                      {key}{" "}
-                    </span>
-                    <span>{value}</span>
-                  </li>
-                ))}
-            </ul>
-            <div className="max-w-3xl overflow-x-scroll">
-              <Table>
-                <Table.Head>
-                  <Table.Row>
-                    {columns.map((column) => (
-                      <Table.Cell key={column}>{column}</Table.Cell>
-                    ))}
-                  </Table.Row>
-                </Table.Head>
-                <Table.Body>
-                  {data
-                    .filter((a) => a.status === "success")
-                    .map((token) => (
-                      <Table.Row key={token.tokenId} className="p-2">
-                        {columns
-                          .map((column) => [
-                            column,
-                            String((token as Record<string, any>)[column]),
-                          ])
-                          .map(([key, value]) => (
-                            <Table.Column
-                              key={key}
-                              className="whitespace-nowrap"
-                            >
-                              {typeof value === "string" &&
-                              value.startsWith("0x") ? (
-                                <CopyToClipboardButton className="flex-nowrap">
-                                  {maskAddress(value as `0x${string}`)}
-                                </CopyToClipboardButton>
-                              ) : (
-                                String(value)
-                              )}
-                            </Table.Column>
-                          ))}
-                      </Table.Row>
-                    ))}
-                </Table.Body>
-              </Table>
-            </div>
-          </Card.Body>
-        </Card>
-      </div>
-    ) : null;
+  const resultSection = useMemo(() => {
+    if (isLoading && isEnabled) {
+      return (
+        <div className="p-4 text-center">
+          Scanning all chains for token {searchValue}...
+        </div>
+      );
+    }
+
+    if (data?.length) {
+      return (
+        <div className="grid">
+          <Card>
+            <Card.Body>
+              <Card.Title>Token Info</Card.Title>
+              <ul>
+                {Object.entries(data[0])
+                  .filter(
+                    ([key, value]) =>
+                      (EXCLUDED_COLUMNS.includes(key) &&
+                        typeof value === "string") ||
+                      typeof value === "number"
+                  )
+                  .map(([key, value]) => (
+                    <li key={key}>
+                      <span className="mr-1 font-semibold opacity-85">
+                        {key}{" "}
+                      </span>
+                      <span>{value}</span>
+                    </li>
+                  ))}
+              </ul>
+              <div className="max-w-3xl overflow-x-scroll">
+                <Table>
+                  <Table.Head>
+                    <Table.Row>
+                      {columns.map((column) => (
+                        <Table.Cell key={column}>{column}</Table.Cell>
+                      ))}
+                    </Table.Row>
+                  </Table.Head>
+                  <Table.Body>
+                    {data
+                      .filter((a) => a.status === "success")
+                      .map((token) => (
+                        <Table.Row key={token.tokenId} className="p-2">
+                          {columns
+                            .map((column) => [
+                              column,
+                              String((token as Record<string, any>)[column]),
+                            ])
+                            .map(([key, value]) => (
+                              <Table.Column
+                                key={key}
+                                className="whitespace-nowrap"
+                              >
+                                {typeof value === "string" &&
+                                value.startsWith("0x") ? (
+                                  <CopyToClipboardButton
+                                    className="flex-nowrap"
+                                    copyText={value}
+                                  >
+                                    {maskAddress(value as `0x${string}`)}
+                                  </CopyToClipboardButton>
+                                ) : (
+                                  String(value)
+                                )}
+                              </Table.Column>
+                            ))}
+                        </Table.Row>
+                      ))}
+                  </Table.Body>
+                </Table>
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      );
+    }
+  }, [columns, data, isEnabled, isLoading, searchValue]);
 
   return (
     <Card className="bg-base-200">
