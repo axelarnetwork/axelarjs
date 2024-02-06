b86e504468d4f51d7b9a93e15377d63139b4ad9a
Canh Trinh <canh@axelar.network>
2024-02-06 08:04:19 -0500

    chore: update rate limit manager text

--- apps/maestro/src/features/ManageInterchainToken/actions/transfer-operatorship/TransferInterchainTokenOperatorship.tsx
+++ apps/maestro/src/features/ManageInterchainToken/actions/transfer-operatorship/TransferInterchainTokenOperatorship.tsx
@@ -157,12 +157,12 @@ export const TransferInterchainTokenOperatorship: FC = () => {
         >
           <FormControl>
             <Label htmlFor="recipientAddress">
-              <Label.Text>Recipient address</Label.Text>
+              <Label.Text>New operator address</Label.Text>
             </Label>
             <TextInput
               id="recipientAddress"
               bordered
-              placeholder="Enter recipient address"
+              placeholder="Enter new operator address"
               className="bg-base-200"
               min={0}
               {...register("recipientAddress", {
