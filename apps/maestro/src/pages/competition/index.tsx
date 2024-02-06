12ac16e0584bb2ea638d441c93d38c1c17567bc1
npty <nptytn2@gmail.com>
2024-02-06 10:37:24 +0700

    chore: adjust color

--- apps/maestro/src/pages/competition/index.tsx
+++ apps/maestro/src/pages/competition/index.tsx
@@ -160,38 +160,38 @@ const CompetitionPage = () => {
       loadingMessage="Loading transactions..."
     >
       <Page.Title>Interchain Leaderboard</Page.Title>
-      <div className="mx-auto max-w-4xl rounded-lg bg-gray-800 p-5 shadow-lg">
-        <h2 className="mb-4 text-xl font-bold text-white">
+      <div className="bg-base-200 mx-auto max-w-4xl rounded-lg p-5 shadow-lg">
+        <h2 className="mb-4 text-xl font-bold text-neutral-300">
           Welcome to the ITS Legend Interchain Competition!
         </h2>
-        <p className="mb-4 text-gray-400">
+        <p className="text-neutral-content mb-4">
           To celebrate the mainnet launch of Interchain Token Service, we are
           launching the ITS Legend Interchain Competition!
         </p>
-        <ul className="list-inside list-none space-y-2 text-gray-400">
+        <ul className="text-neutral-content list-inside list-none space-y-2">
           <li>
-            <strong className="text-white">Objective:</strong> Mint new
+            <strong className="text-neutral-300">Objective:</strong> Mint new
             Interchain Tokens and drive as many transactions as possible with
             them.
           </li>
           <li>
-            <strong className="text-white">Duration:</strong> The competition
-            kicks off at 1pm EST on Feb 7 and concludes at 1pm EST on Feb 21,
-            spanning a thrilling two weeks.
+            <strong className="text-neutral-300">Duration:</strong> The
+            competition kicks off at 1pm EST on Feb 7 and concludes at 1pm EST
+            on Feb 21, spanning a thrilling two weeks.
           </li>
           <li>
-            <strong className="text-white">Prizes:</strong> The top three
+            <strong className="text-neutral-300">Prizes:</strong> The top three
             projects with the highest transaction counts will be rewarded with
             AXL tokens:
-            <ul className="list-disc space-y-1 pl-5">
-              <li className="text-gray-400">1st Place: 10,101 AXL</li>
-              <li className="text-gray-400">2nd Place: 6,900 AXL</li>
-              <li className="text-gray-400">3rd Place: 4,200 AXL</li>
+            <ul className="text-neutral-content list-disc space-y-1 pl-5">
+              <li>1st Place: 10,101 AXL</li>
+              <li>2nd Place: 6,900 AXL</li>
+              <li>3rd Place: 4,200 AXL</li>
             </ul>
           </li>
           <li>
-            <strong className="text-white">Eligibility:</strong> Only tokens
-            minted after the competition starts are eligible.
+            <strong className="text-neutral-300">Eligibility:</strong> Only
+            tokens minted after the competition starts are eligible.
           </li>
         </ul>
       </div>
