4e485cf4fdc0ca1bd007e4591031a54ca9edc888
npty <nptytn2@gmail.com>
2024-02-06 20:47:34 +0700

    chore: remove log from hooks

--- apps/maestro/src/services/axelarjsSDK/hooks.ts
+++ apps/maestro/src/services/axelarjsSDK/hooks.ts
@@ -8,7 +8,6 @@ import type {
 const staleTime = 1000 * 60 * 2; // 2 minutes
 
 export function useEstimateGasFeeQuery(input: EstimateGasFeeInput) {
-  console.log("input", input);
   return trpc.axelarjsSDK.estimateGasFee.useQuery(input, {
     staleTime,
     enabled:
