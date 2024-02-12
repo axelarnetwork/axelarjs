778db54fda580e2b9da3eafee3a4befebd99ae12
Alan Soares <alanrsoars@gmail.com>
2024-02-12 20:16:23 +1300

    chore: upgrade remaining deps

--- apps/maestro/vitest.config.ts
+++ apps/maestro/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-expect-error mismatching types
   plugins: [react()],
   test: {
     environment: "happy-dom",
