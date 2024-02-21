237030d92912e3de873a147c67f37d2ee937239c
Alan Soares <alanrsoars@gmail.com>
2024-02-22 10:38:28 +1300

    fix: apply trpc-openapi patch to fix integration with trpc v11

--- apps/maestro/src/pages/api/openapi.json.ts
+++ apps/maestro/src/pages/api/openapi.json.ts
@@ -4,6 +4,7 @@ import { openApiDocument } from "~/server/openapi";
 
 // Respond with our OpenAPI schema
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  console.log("openApiDocument", openApiDocument);
   res.status(200).send(openApiDocument);
 };
 
