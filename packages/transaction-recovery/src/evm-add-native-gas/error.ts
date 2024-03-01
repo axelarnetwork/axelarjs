f2ea05f237357cae1aaca3dd77657e2dffb0d928
npty <nptytn2@gmail.com>
2024-03-01 17:26:27 +0700

    chore: reorder

--- packages/transaction-recovery/src/evm-add-native-gas/error.ts
+++ packages/transaction-recovery/src/evm-add-native-gas/error.ts
@@ -2,10 +2,9 @@ export const EvmAddNativeGasError = {
   NO_PAYMENT_REQUIRED: new Error("This transaction does not require gas paid"),
   ENOUGH_PAID: new Error("This transaction already has paid enough gas"),
   INVALID_GMP_TX: new Error("Invalid GMP Tx"),
+  REFUND_ADDRESS_NOT_FOUND: "Refund address not found",
   GAS_SERVICE_ADDRESS_NOT_FOUND: (chain: string) =>
     new Error(`Gas service address not found for ${chain}`),
-  REFUND_ADDRESS_NOT_FOUND: "Refund address not found",
-  SENDER_ADDRESS_NOT_FOUND: "Sender address not found",
   CHAIN_CONFIG_NOT_FOUND: (chain: string) =>
     new Error(`Chain config not found ${chain}`),
   WALLET_CLIENT_NOT_FOUND: new Error(
