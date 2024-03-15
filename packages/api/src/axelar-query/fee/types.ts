67fd56c5c357e1496cd36621df927de6f7a6ed36
npty <nptytn2@gmail.com>
2024-03-15 15:58:38 +0700

    chore: remove `destinationContractAddress` from EstimateL1FeeParams

--- packages/api/src/axelar-query/fee/types.ts
+++ packages/api/src/axelar-query/fee/types.ts
@@ -1,7 +1,6 @@
 import { TokenUnit, type L2Type } from "../../gmp";
 
 export type EstimateL1FeeParams = {
-  destinationContractAddress?: `0x${string}` | undefined;
   executeData: `0x${string}`;
   l1GasPrice: TokenUnit;
   l1GasOracleAddress?: `0x${string}` | undefined;
