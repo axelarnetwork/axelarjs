ca8b7c4be8db8e603075dc460e379a3346b9e799
Alan Soares <alanrsoars@gmail.com>
2024-03-18 14:10:38 +1300

    chore: read swagger api version from app version

--- apps/maestro/src/server/openapi.ts
+++ apps/maestro/src/server/openapi.ts
@@ -1,13 +1,14 @@
 import { generateOpenApiDocument } from "trpc-openapi";
 
 import { getBaseUrl } from "~/lib/trpc";
+import { version } from "../../package.json";
 import { appRouter } from "./routers/_app";
 
 // Generate OpenAPI schema document
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "Interchain Token Service API",
   description: "Interchain Token Service API, search interchain tokens",
-  version: "1.0.0",
+  version: version,
   baseUrl: `${getBaseUrl()}/api`,
   docsUrl: "https://github.com/axelarnetwork/axelarjs/tree/main/apps/maestro",
   tags: ["interchain-token", "abi", "gmp"],
