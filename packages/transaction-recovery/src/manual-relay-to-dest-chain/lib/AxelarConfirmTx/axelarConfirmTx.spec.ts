a76dc76df63cd52cd34b403ae0893142a53973db
npty <nptytn2@gmail.com>
2024-05-30 15:19:38 +0700

    chore: add tests for axelarConfirmTx

--- packages/transaction-recovery/src/manual-relay-to-dest-chain/lib/AxelarConfirmTx/axelarConfirmTx.spec.ts
+++ packages/transaction-recovery/src/manual-relay-to-dest-chain/lib/AxelarConfirmTx/axelarConfirmTx.spec.ts
@@ -10,22 +10,37 @@ import {
 } from "@axelarjs/api";
 import { createAxelarRPCQueryClient } from "@axelarjs/cosmos";
 
+import { skip } from "node:test";
+import type { DeliverTxResponse } from "@cosmjs/stargate";
 import { hashMessage } from "viem";
 
-import { sendAxelarConfirmTx } from ".";
+import { sendAxelarConfirmTx, type SendAxelarConfirmTxDependencies } from ".";
+import {
+  Event,
+  Event_Status,
+} from "../../../../../proto/build/module/axelar/evm/v1beta1/types";
 import { ConfirmGatewayTxError } from "./error";
 import * as Qualifier from "./qualifier";
 
 describe("AxelarConfirmTx", () => {
   const environment = "testnet";
   const txHash = hashMessage("random tx hash");
+  const searchGMPData = {
+    call: {
+      transactionHash: txHash,
+      blockNumber: 1,
+    },
+  };
 
   const deps = {
     axelarscanClient: createAxelarscanClient(environment),
     axelarRecoveryApiClient: createAxelarRecoveryApiClient(environment),
     configClient: createAxelarConfigClient(environment),
     gmpClient: createGMPClient(environment),
+    axelarQueryRpcClient: undefined,
   };
+  let confirmDeps: SendAxelarConfirmTxDependencies;
+
   const mockConfig = {
     chain_type: "evm" as const,
     id: "test",
@@ -33,35 +48,115 @@ describe("AxelarConfirmTx", () => {
     rpcUrl: "http://localhost:26657",
   };
 
-  test.only("should returns error if not reach finalized block", async () => {
+  const params = {
+    srcChainConfig: mockConfig,
+    searchGMPData: searchGMPData as SearchGMPResponseData,
+  };
+
+  beforeEach(async () => {
     const axelarQueryRpcClient = await createAxelarRPCQueryClient({
       environment,
     });
-
-    const searchGMPData = {
-      call: {
-        transactionHash: txHash,
-        blockNumber: 1,
-      },
+    confirmDeps = {
+      ...deps,
+      axelarQueryRpcClient,
     };
+  });
 
+  test("should returns error if not reach finalized block", async () => {
     vitest.spyOn(Qualifier, "isBlockFinalized").mockResolvedValueOnce(false);
+
+    const response = await sendAxelarConfirmTx(params, confirmDeps);
+
+    expect(response).toMatchObject({
+      skip: true,
+      type: "axelar.confirm_gateway_tx",
+      error: ConfirmGatewayTxError.NOT_FINALIZED(txHash).message,
+    });
+  });
+
+  test("should returns error if failed to fetch EVM event or event not found", async () => {
+    vitest.spyOn(Qualifier, "isBlockFinalized").mockResolvedValueOnce(true);
+    vitest
+      .spyOn(confirmDeps.axelarQueryRpcClient.evm, "Event")
+      .mockRejectedValueOnce(new Error("error"));
+
+    const response = await sendAxelarConfirmTx(params, confirmDeps);
+
+    expect(response).toMatchObject({
+      skip: true,
+      type: "axelar.confirm_gateway_tx",
+      error: ConfirmGatewayTxError.FAILED_FETCH_EVM_EVENT.message,
+    });
+
+    vitest.spyOn(Qualifier, "isBlockFinalized").mockResolvedValueOnce(true);
+    vitest
+      .spyOn(confirmDeps.axelarQueryRpcClient.evm, "Event")
+      .mockResolvedValueOnce({ event: undefined });
+
+    const response2 = await sendAxelarConfirmTx(params, confirmDeps);
+
+    expect(response2).toMatchObject({
+      skip: true,
+      type: "axelar.confirm_gateway_tx",
+      error: ConfirmGatewayTxError.EVM_EVENT_NOT_FOUND.message,
+    });
+  });
+
+  test("should returns skip: true if the tx is already confirmed", async () => {
+    vitest.spyOn(Qualifier, "isBlockFinalized").mockResolvedValueOnce(true);
+    const confirmEvent = Event.create({
+      chain: "test",
+      txId: new Uint8Array(0),
+      index: 1,
+      status: Event_Status.STATUS_COMPLETED,
+    });
+    vitest
+      .spyOn(confirmDeps.axelarQueryRpcClient.evm, "Event")
+      .mockResolvedValueOnce({
+        event: confirmEvent,
+      });
+    const response = await sendAxelarConfirmTx(params, confirmDeps);
+
+    expect(response).toMatchObject({
+      skip: true,
+      type: "axelar.confirm_gateway_tx",
+    });
+  });
+
+  test("should returns success if all conditions are met", async () => {
+    vitest.spyOn(Qualifier, "isBlockFinalized").mockResolvedValueOnce(true);
+    const confirmEvent = Event.create({
+      chain: "test",
+      txId: new Uint8Array(0),
+      index: 1,
+      status: Event_Status.STATUS_UNSPECIFIED,
+    });
+    vitest
+      .spyOn(confirmDeps.axelarQueryRpcClient.evm, "Event")
+      .mockResolvedValueOnce({
+        event: confirmEvent,
+      });
+    vitest
+      .spyOn(confirmDeps.axelarRecoveryApiClient, "confirm")
+      .mockResolvedValueOnce({
+        transactionHash: txHash,
+      } as DeliverTxResponse);
 
     const response = await sendAxelarConfirmTx(
       {
         searchGMPData: searchGMPData as SearchGMPResponseData,
         srcChainConfig: mockConfig,
       },
-      {
-        ...deps,
-        axelarQueryRpcClient,
-      }
+      confirmDeps
     );
 
     expect(response).toMatchObject({
-      skip: true,
+      skip: false,
       type: "axelar.confirm_gateway_tx",
-      error: ConfirmGatewayTxError.NOT_FINALIZED(txHash).message,
+      tx: {
+        hash: txHash,
+      },
     });
   });
 });
