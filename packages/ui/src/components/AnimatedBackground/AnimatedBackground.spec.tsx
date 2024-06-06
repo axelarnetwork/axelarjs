6776a0a8fbbab9512a2f80bff627b06c76522a74
Santiago Giaccobasso <santigiacco@192.168.0.104>
2024-06-06 17:13:25 -0300

    feat: animated bg in ui package

--- packages/ui/src/components/AnimatedBackground/AnimatedBackground.spec.tsx
+++ packages/ui/src/components/AnimatedBackground/AnimatedBackground.spec.tsx
@@ -1,11 +1,11 @@
 import { composeStories } from "@storybook/react";
 import { render } from "@testing-library/react";
 
-import * as stories from "./AnimatedBlobBackground.stories";
+import * as stories from "./AnimatedBackground.stories";
 
 const { Default } = composeStories(stories);
 
-describe("AnimatedBlobBackground", () => {
+describe("AnimatedBackground", () => {
   it("renders Default component story without breaking", () => {
     const { container } = render(<Default />);
     expect(container).toBeVisible();
