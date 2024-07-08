b49ad47cf25df9077aa3213ceae99fd5fa452bfc
Santiago Giaccobasso <sgiaccobasso@gmail.com>
2024-07-08 14:56:54 -0300

    feat: create a show only once modal (#391)
    * feat: create a show only once modal
    
    * fix: change image colors
    
    ---------
    
    Co-authored-by: Santiago Giaccobasso <santigiacco@192.168.0.105>

--- apps/maestro/src/pages/index.tsx
+++ apps/maestro/src/pages/index.tsx
@@ -10,6 +10,7 @@ import SearchInterchainToken, {
   TokenFoundResult,
 } from "~/features/SearchInterchainToken";
 import { ConditionalRenderInterchainBanner } from "~/ui/components/InterchainBanner";
+import ShowOnceModal from "~/ui/components/ShowOnceModal";
 import { useLayoutStateContainer } from "~/ui/layouts/MainLayout";
 import Page from "~/ui/layouts/Page";
 
@@ -51,6 +52,7 @@ export default function HomePage() {
         <div className="w-full">
           <ConditionalRenderInterchainBanner />
         </div>
+        <ShowOnceModal />
 
         <div
           className="flex min-h-[80dvh] w-full max-w-lg flex-col items-center justify-center"
