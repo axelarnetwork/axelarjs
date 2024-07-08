4b8291f9d3fe5eb854ed84563dfc81a95e42ffce
Santiago Giaccobasso <sgiaccobasso@gmail.com>
2024-07-08 13:33:55 -0300

    feat: add twitter conversion tracking base code (#390)
    Co-authored-by: Santiago Giaccobasso <santigiacco@192.168.0.105>

--- apps/maestro/src/pages/_document.tsx
+++ apps/maestro/src/pages/_document.tsx
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="light" className="scroll-smooth">
       <Head>
+        {/* Twitter conversion tracking base code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','omouz');`,
+          }}
+        />
+        {/* End Twitter conversion tracking base code */}
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.png" />
