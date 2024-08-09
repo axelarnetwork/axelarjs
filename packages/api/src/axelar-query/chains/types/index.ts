552084a1233f9b55eb39a396a27f4c11defcd398
Santiago Giaccobasso <santigiacco@192.168.1.6>
2024-08-09 15:49:27 -0300

    chore: remove unused types

--- packages/api/src/axelar-query/chains/types/index.ts
+++ packages/api/src/axelar-query/chains/types/index.ts
@@ -1,16 +1,5 @@
 import type { Environment } from "../../..";
 
-export interface Chain {
-  chainInfo: ChainInfo;
-  validateAddress: (destinationAddress: string) => boolean;
-}
-
-export interface AssetAndChainInfo {
-  assetInfo: AssetInfoResponse;
-  sourceChainInfo: ChainInfo;
-  destinationChainInfo: ChainInfo;
-}
-
 export interface ChainInfo {
   id: string;
   assets: AssetInfo[];
@@ -29,60 +18,6 @@ export interface ChainInfo {
   nativeAsset: string[];
   addressPrefix: string;
   channelIdToAxelar?: string;
-}
-
-export interface AssetTransferObject {
-  sourceChainInfo: ChainInfo;
-  destinationChainInfo: ChainInfo;
-  selectedSourceAsset: AssetInfo;
-  selectedDestinationAsset: AssetInfo;
-  signature: string;
-  publicAddr: string;
-  transactionTraceId?: string;
-}
-
-export interface AssetInfoWithTrace extends AssetInfo {
-  traceId: string;
-  assetInfo: AssetInfo;
-}
-
-export type SourceOrDestination = "source" | "destination";
-
-export interface AssetInfoResponse extends AssetInfo {
-  sourceOrDestChain: SourceOrDestination;
-  traceId: string;
-}
-
-export enum LinkType {
-  EVM = "/axelar.evm.v1beta1.LinkRequest",
-  COS = "/axelar.axelarnet.v1beta1.LinkRequest",
-}
-
-export interface LinkRequestBody {
-  "@type": LinkType;
-  recipient_addr: string;
-  recipient_chain: string;
-}
-
-export interface EVMLinkRequestBody extends LinkRequestBody {
-  chain: string; //source chain
-  asset: string;
-}
-
-export interface COSLinkRequestBody extends LinkRequestBody {
-  asset: string;
-}
-
-// for connections from ui >> bridge server
-export enum SocketListenerTypes {
-  /*axelarnet listener for deposit event*/
-  LINK = "LINK",
-  WAIT_FOR_DEPOSIT = "WAIT_FOR_DEPOSIT",
-  DEPOSIT_CONFIRMED = "DEPOSIT_CONFIRMED",
-}
-
-export interface SocketListenerTopic {
-  topic: SocketListenerTypes;
 }
 
 export type LoadChainConfig = {
