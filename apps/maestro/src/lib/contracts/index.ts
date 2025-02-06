38d5148d7372ffd04808d440cee8252ed69ffb0a
npty <nptytn2@gmail.com>
2025-02-06 11:42:55 +0700

    chore: install its 2.1.0 and regenerate abis and hooks

--- apps/maestro/src/lib/contracts/index.ts
+++ apps/maestro/src/lib/contracts/index.ts
@@ -18,12 +18,12 @@ export const contracts = [
   {
     name: INTERCHAIN_TOKEN_FACTORY_ABI.contractName,
     abi: INTERCHAIN_TOKEN_FACTORY_ABI.abi,
-    address: "0x6Ae8C8498d5FDA930e6ABeB0E15E5A00471702a7" as `0x${string}`, // read from .env.local (NEXT_PUBLIC_INTERCHAIN_TOKEN_FACTORY_ADDRESS)
+    address: "0xdB7d6A5B8d37a4f34BC1e7ce0d0B8a9DDA124871" as `0x${string}`, // read from .env.local (NEXT_PUBLIC_INTERCHAIN_TOKEN_FACTORY_ADDRESS)
   },
   {
     name: INTERCHAIN_TOKEN_SERVICE_ABI.contractName,
     abi: INTERCHAIN_TOKEN_SERVICE_ABI.abi,
-    address: "0x144c3d7A5f5198EF3B46A8258b35E903cf197A66" as `0x${string}`, // read from .env.local (NEXT_PUBLIC_INTERCHAIN_TOKEN_SERVICE_ADDRESS)
+    address: "0x2269B93c8D8D4AfcE9786d2940F5Fcd4386Db7ff" as `0x${string}`, // read from .env.local (NEXT_PUBLIC_INTERCHAIN_TOKEN_SERVICE_ADDRESS)
   },
   {
     name: TOKEN_MANAGER_ABI.contractName,
