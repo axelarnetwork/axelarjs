04aaafa78feb4e41d54b061a773116d7aefea97f
SGiaccobasso <sgiaccobasso@gmail.com>
2025-02-18 23:14:44 -0300

    chore: switch chains seamlessly

--- apps/maestro/src/lib/hooks/useSwitchChain.ts
+++ apps/maestro/src/lib/hooks/useSwitchChain.ts
@@ -1,7 +1,13 @@
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+
+import { useConnectWallet, useWallets } from "@mysten/dapp-kit";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { useSwitchChain as useWagmiSwitchChain } from "wagmi";
 
 import { NEXT_PUBLIC_NETWORK_ENV } from "~/config/env";
 import { useChainId, useDisconnect } from "~/lib/hooks";
+import { isValidEVMAddress } from "../utils/validation";
 
 // TODO: fixing this once we have the sui chain data
 const CHAIN_ID_SUI = NEXT_PUBLIC_NETWORK_ENV === "mainnet" ? 101 : 103;
@@ -10,22 +16,63 @@ export function useSwitchChain() {
   const { switchChain: switchChainWagmi } = useWagmiSwitchChain();
   const { disconnect } = useDisconnect();
   const currentChainId = useChainId();
+  const wallets = useWallets();
+  const { mutate: connect } = useConnectWallet();
+  const { open: openWeb3Modal } = useWeb3Modal();
+  const { data: sessionData } = useSession();
+  const [pendingChainId, setPendingChainId] = useState<number | null>(null);
 
-  function switchChain({ chainId }: { chainId: number }) {
+  useEffect(() => {
+    if (
+      sessionData?.address.length &&
+      isValidEVMAddress(sessionData?.address) &&
+      pendingChainId
+    ) {
+      switchChainWagmi({ chainId: pendingChainId });
+      setPendingChainId(null);
+    }
+  }, [sessionData, pendingChainId, switchChainWagmi, setPendingChainId]);
+
+  const tryConnectSuiWallet = async () => {
+    for (const wallet of wallets) {
+      try {
+        // Attempt to connect to each wallet
+        await new Promise<void>((resolve, reject) => {
+          connect(
+            { wallet },
+            {
+              onSuccess: () => resolve(),
+              onError: (error) => reject(error),
+            }
+          );
+        });
+        return true;
+      } catch (error) {
+        continue;
+      }
+    }
+    return false;
+  };
+
+  async function switchChain({ chainId }: { chainId: number }) {
     const isTargetChainSui = chainId === CHAIN_ID_SUI;
     const isCurrentChainSui = currentChainId === CHAIN_ID_SUI;
 
     if (isTargetChainSui && !isCurrentChainSui) {
       disconnect();
+      await tryConnectSuiWallet();
     } else if (!isTargetChainSui && isCurrentChainSui) {
       disconnect();
-      switchChainWagmi({ chainId });
-    } else if (!isTargetChainSui && !isCurrentChainSui) {
+      setPendingChainId(chainId);
+      await openWeb3Modal();
+    } else if (!isTargetChainSui && !isCurrentChainSui && chainId) {
       switchChainWagmi({ chainId });
     }
   }
+
   return {
     switchChain,
+    tryConnectSuiWallet,
   };
 }
 
