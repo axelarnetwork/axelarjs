8996e1c22e4db7db5a3388b3f5c416860af46701
npty <nptytn2@gmail.com>
2025-03-27 14:57:56 +0700

    chore: fix wrong imports

--- packages/sui/src/index.ts
+++ packages/sui/src/index.ts
@@ -1,6 +1,6 @@
 import * as MainnetContracts from './mainnet/types'
-import * as TestnetContracts from './devnet-amplifier/types'
-import * as DevnetAmplifierContracts from './testnet/types'
+import * as TestnetContracts from './testnet/types'
+import * as DevnetAmplifierContracts from './devnet-amplifier/types'
 
 export default {
   mainnet: MainnetContracts,
