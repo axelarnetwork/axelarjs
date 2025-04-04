d64391654f142443a6a74099564c2ec39321e31c
Zachary Brandt <zachary.brandt@gmail.com>
2025-04-04 12:35:47 -0700

    chore: create bridge page, install SDK

---
+++ apps/maestro/src/pages/squid.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// import { Squid } from "@0xsquid/sdk"; // Import Squid SDK
+
+// // Initialize the Squid client with the base URL and integrator ID
+// const getSDK = (): Squid => {
+//   const squid = new Squid({
+//     baseUrl: "https://apiplus.squidrouter.com",
+//     integratorId: integratorId,
+//   });
+//   return squid;
+// };
+
+export default function Bridge() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
