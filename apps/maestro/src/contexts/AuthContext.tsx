e9fdea6be44ea5d7a99ced125e20b21accb68c05
SGiaccobasso <sgiaccobasso@gmail.com>
2025-05-04 16:57:25 -0300

    chore: comments in auth context

--- apps/maestro/src/contexts/AuthContext.tsx
+++ apps/maestro/src/contexts/AuthContext.tsx
@@ -1,3 +1,5 @@
+// This context is used to provide signInState and have only one instance of useWeb3SignIn.
+// Needed to prevent multiple signature requests.
 import React, { createContext, useContext } from "react";
 
 import { useWeb3SignIn } from "~/lib/auth/useWeb3SignIn";
@@ -6,8 +8,6 @@ type AuthContextType = ReturnType<typeof useWeb3SignIn>;
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-// This is a provider for the AuthContext. It is used to provide the signInState to the children and have only one instance of the signInState.
-// Needed because useWeb3SignIn is a hook and would create a new instance of the hook on every render, triggering multiple sign in attempts.
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const signInState = useWeb3SignIn();
   return (
