d39688c8a6e2fad1a4bc628211638454240eca42
SGiaccobasso <sgiaccobasso@gmail.com>
2025-05-06 20:56:37 -0300

    chore: let users switch to stellar from token card

--- apps/maestro/src/features/InterchainTokenList/RegisteredInterchainTokenCard.tsx
+++ apps/maestro/src/features/InterchainTokenList/RegisteredInterchainTokenCard.tsx
@@ -193,22 +193,7 @@ export const RegisteredInterchainTokenCard: FC<Props> = (props) => {
         {!balance?.tokenBalance ? (
           !address ? null : (
             <div>
-              {props.chain?.id?.toLowerCase().includes("stellar") ? (
-                <LinkButton
-                  $size="xs"
-                  $variant="primary"
-                  className="my-1 flex w-full items-center justify-center gap-2"
-                  href={explorerUrl}
-                  target="_blank"
-                >
-                  View on Stellar Explorer{" "}
-                  <ChainIcon
-                    src={props.chain?.image ?? ""}
-                    size="xs"
-                    alt={props.chain?.name ?? ""}
-                  />
-                </LinkButton>
-              ) : isIncompatibleChain ? (
+              {isIncompatibleChain ? (
                 <Button
                   $size="xs"
                   $variant="primary"
