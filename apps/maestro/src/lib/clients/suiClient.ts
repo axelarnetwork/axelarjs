03ac0102e582ade16bc6b840394d2a4eb51d3abb
SGiaccobasso <sgiaccobasso@gmail.com>
2025-05-20 19:30:44 -0300

    fix: stellar links (#596)
    Co-authored-by: Victor Henrique Collasanta <96581579+collasanta@users.noreply.github.com>

--- apps/maestro/src/lib/clients/suiClient.ts
+++ apps/maestro/src/lib/clients/suiClient.ts
@@ -1,7 +1,9 @@
 import { SUI_RPC_URLS } from "@axelarjs/core";
+
 import { SuiClient } from "@mysten/sui/client";
 import { SuiGraphQLClient } from "@mysten/sui/graphql";
-import { graphql } from '@mysten/sui/graphql/schemas/latest';
+import { graphql } from "@mysten/sui/graphql/schemas/latest";
+
 import { NEXT_PUBLIC_NETWORK_ENV } from "~/config/env";
 
 // Initialize SuiClient directly with RPC from config
@@ -17,6 +19,6 @@ const SUI_GRAPHQL_URLS = {
 
 export const suiGraphQLClient = new SuiGraphQLClient({
   url: SUI_GRAPHQL_URLS[NEXT_PUBLIC_NETWORK_ENV],
-})
+});
 
-export const suiQuery = graphql
+export const suiQuery = graphql;
