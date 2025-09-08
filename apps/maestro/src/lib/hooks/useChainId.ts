ac220701ad5ac014163500b206552edb6d5b4c77
Dominik Apel <53536302+dfapel@users.noreply.github.com>
2025-09-08 11:04:31 +0200

    feat(xrpl): finish src/lib/hooks, add adapters

--- apps/maestro/src/lib/hooks/useChainId.ts
+++ apps/maestro/src/lib/hooks/useChainId.ts
@@ -7,16 +7,24 @@ import { useChainId as useWagmiChainId } from "wagmi";
 import { NEXT_PUBLIC_NETWORK_ENV } from "~/config/env";
 import { getStellarConnectionState } from "~/lib/utils/stellar";
 import { useAccount } from "./useAccount";
+import { useWallet as useXRPLWallet } from "@xrpl-wallet-standard/react";
 
 export const SUI_CHAIN_ID = NEXT_PUBLIC_NETWORK_ENV === "mainnet" ? 101 : 103;
 export const STELLAR_CHAIN_ID =
   NEXT_PUBLIC_NETWORK_ENV === "mainnet" ? 109 : 110;
+export const XRPL_CHAIN_ID =
+  NEXT_PUBLIC_NETWORK_ENV === "mainnet"
+    ? 114
+    : NEXT_PUBLIC_NETWORK_ENV === "devnet-amplifier"
+      ? 116
+      : 115;
 
 // TODO: check if this is the best way to use chain ids, maybe we should combine it with chain type
 export function useChainId(): number {
   const wagmiChainId = useWagmiChainId();
   const suiAccount = useCurrentAccount();
   const { chain } = useAccount();
+  const XRPLWallet = useXRPLWallet();
 
   const chainId = useMemo(() => {
     // Check if Stellar wallet is connected
@@ -24,6 +32,10 @@ export function useChainId(): number {
       // this saves us from having to listen to the storage event but we could find a better way
       return STELLAR_CHAIN_ID;
     }
+    if (chain?.id === XRPL_CHAIN_ID) {
+      return XRPL_CHAIN_ID;
+    }
+
     const isStellarConnected = getStellarConnectionState() ?? false;
     if (isStellarConnected) {
       return STELLAR_CHAIN_ID;
@@ -31,8 +43,14 @@ export function useChainId(): number {
     if (suiAccount) {
       return SUI_CHAIN_ID;
     }
+
+    // Check if XRPL wallet is connected // TODO: why?
+    if (XRPLWallet.status === "connected" && (XRPLWallet.wallet?.accounts.length ?? -1 > 0)) {
+      return XRPL_CHAIN_ID;
+    }
+
     return wagmiChainId;
-  }, [wagmiChainId, suiAccount, chain]);
+  }, [wagmiChainId, suiAccount, chain, XRPLWallet]);
 
   return chainId;
 }
