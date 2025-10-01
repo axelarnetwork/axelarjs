f9840bf5dad73a785f499277b20508cfd9867bde
Dominik Apel <53536302+dfapel@users.noreply.github.com>
2025-10-01 22:28:01 +0300

    feat: add connect XRPL wallet

---
+++ apps/maestro/src/ui/compounds/XRPLWalletList/WalletList.tsx
@@ -0,0 +1,65 @@
+import type { XRPLWallet } from '@xrpl-wallet-standard/app'
+import styled from 'styled-components'
+import { useConnect, useWallets } from '@xrpl-wallet-standard/react'
+import { getRegisterdXRPLWallets } from "@xrpl-wallet-standard/app";
+
+const WalletListContainer = styled.ul`
+  padding: 0;
+  margin: 0;
+`
+
+const WalletItem = styled.li`
+  list-style: none;
+  display: flex;
+`
+
+const WalletButton = styled.button`
+  display: flex;
+  width: 100%;
+  justify-content: stretch;
+  align-items: center;
+  gap: 1rem;
+  padding: 1.2rem;
+  margin: 0.3rem;
+`
+
+const WalletIcon = styled.img`
+  height: 36px;
+  vertical-align: middle;
+`
+
+const WalletName = styled.span`
+  vertical-align: middle;
+`
+
+type Props = {
+  onConnectSuccess: () => void
+  onConnectError: (error: Error) => void
+}
+
+export const WalletList = ({ onConnectSuccess, onConnectError }: Props) => {
+  const wallets = getRegisterdXRPLWallets()
+  const { connect } = useConnect()
+
+  const handleConnect = async (wallet: XRPLWallet) => {
+    try {
+      await connect(wallet)
+      onConnectSuccess()
+    } catch (error: any) {
+      onConnectError(error)
+    }
+  }
+
+  return (
+    <WalletListContainer>
+      {wallets.map((wallet) => (
+        <WalletItem key={wallet.name}>
+          <WalletButton onClick={() => handleConnect(wallet)}>
+            <WalletIcon src={wallet.icon} alt={wallet.name} />
+            <WalletName>{wallet.name}</WalletName>
+          </WalletButton>
+        </WalletItem>
+      ))}
+    </WalletListContainer>
+  )
+}
