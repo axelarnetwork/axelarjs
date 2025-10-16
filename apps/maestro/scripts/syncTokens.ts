68431a79ecad7c26043f59efa489d8625fe84c19
Dominik Apel <53536302+dfapel@users.noreply.github.com>
2025-10-16 17:12:14 +0200

    chore: add imported flag to database, allow to purge imported tokens, cleanup

--- apps/maestro/scripts/syncTokens.ts
+++ apps/maestro/scripts/syncTokens.ts
@@ -1,41 +1,34 @@
 import { config } from "dotenv";
 config({ path: ".env.local" }); // Load variables from .env.local
 import { Client } from "pg";
-import https from "https";
-import http from "http";
-
-//import { CHAIN_CONFIGS } from "~/config/chains"; // Import doesn't work
-
-function fetchJson(url: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-    let protocol: any = http;
-    if(url.startsWith("https"))
-      protocol = https;
-    protocol.get(url, (res) => {
-      let data = "";
-      res.on("data", (chunk) => (data += chunk));
-      res.on("end", () => {
-        try {
-          resolve(JSON.parse(data));
-        } catch (e) {
-          reject(e);
-        }
-      });
-    }).on("error", reject);
-  });
-}
+import { createAxelarConfigClient } from "../../../packages/api/axelar-config";
 
 async function main() {
+  // Load axelar config
+  const {
+    NEXT_PUBLIC_NETWORK_ENV
+  } = process.env;
+
+  if (!NEXT_PUBLIC_NETWORK_ENV) {
+    throw new Error("Missing required environment variable NEXT_PUBLIC_NETWORK_ENV");
+  }
+
+  if (!["testnet", "mainnet", "devnet-amplifier"].includes(NEXT_PUBLIC_NETWORK_ENV)) {
+    throw new Error("Invalid network env");
+  }
+
+  const axelarConfigClient = createAxelarConfigClient(NEXT_PUBLIC_NETWORK_ENV as any);
+  const config = await axelarConfigClient.getAxelarConfigs();
+
   // Load DB credentials from env
   const {
     POSTGRES_USER,
     POSTGRES_HOST,
     POSTGRES_PASSWORD,
     POSTGRES_DATABASE,
-    CONFIG_FILE_URL,
   } = process.env;
   
-  if (!POSTGRES_USER || !POSTGRES_HOST || !POSTGRES_PASSWORD || !POSTGRES_DATABASE || !CONFIG_FILE_URL) {
+  if (!POSTGRES_USER || !POSTGRES_HOST || !POSTGRES_PASSWORD || !POSTGRES_DATABASE) {
     throw new Error("Missing required environment variables");
   }
 
@@ -50,14 +43,26 @@ async function main() {
 
   await client.connect();
 
-  // Fetch config
-  const config = await fetchJson(CONFIG_FILE_URL);
-
   // Extract asset information
   const assets = config?.assets;
   if (!assets || typeof assets !== "object") {
-    throw new Error("XRPL assets not found in config");
+    throw new Error("Assets not found in config");
   }
+
+  // did the user request to purge all imported tokens?
+  const deleteImported = process.argv.includes("--delete-imported");
+  if (deleteImported) {
+    console.log("Deleting all imported tokens before syncing");
+    try {
+      client.query("DELETE FROM remote_interchain_tokens WHERE imported = true");
+      client.query("DELETE FROM interchain_tokens WHERE imported = true");
+    }
+    catch (err) {
+      console.error("Failed to delete tokens:", err);
+      throw err;
+    }
+  }
+
 
   // I want to query everything from table interchain_tokens
   let currentTokens;
@@ -67,26 +72,29 @@ async function main() {
       acc[row.token_id] = row;
       return acc;
     }, {} as Record<string, any>);
-    console.log("Current interchain_tokens:", currentTokens);
   } catch (err) {
     console.error("Error querying interchain_tokens:", err);
     await client.end();
-    return null;
+    throw err;
   }
 
+  let tokensAdded = [];
+  let tokensIgnored = [];
+  let tokensAlreadyPresent = [];
+
   for (const [tokenId, tokenInfo] of Object.entries(assets)) {
-    // check if this token is a custom token
-    if(tokenInfo.type != "customInterchain" && tokenInfo.type != "interchain") {//if (!/^0x[a-fA-F0-9]{64}$/.test(tokenId)) {
-      //console.log(`Skipping token ${tokenId} of type ${tokenInfo.type}`);
+    // check if this token is unsupported
+    if(tokenInfo.type != "customInterchain" && tokenInfo.type != "interchain") {
+      tokensIgnored.push(tokenId);
       continue;
     }
     if (tokenInfo.type == "customInterchain") {
       tokenInfo.type = "interchain"; // normalize to interchain
     }
 
-    // check if this token is already in the interchain_tokens database
+    // check if this token is already in the interchain_tokens table
     if (currentTokens[tokenId]) {
-      console.log(`Token ${tokenId} already exists in interchain_tokens, skipping`);
+      tokensAlreadyPresent.push(tokenId);
       continue;
     }
 
@@ -94,22 +102,49 @@ async function main() {
     // the format is token_id, token_address, axelar_chain_id, token_name, token_symbol, token_decimals, deployment_message_id, deployer_address, token_manager_address, token_manager_type, original_minter_address, kind, salt, updated_at, created_at
     // this is an example:
     /*
-      id: '0xcf24134c93dcca4c1cfa6595dc337849d9e9e5507c3cbffd937d617e015e81f9',
-      prettySymbol: 'YSLC',
-      name: 'Slime Coin',
-      decimals: 18,
-      originAxelarChainId: 'Moonbeam',
-      coingeckoId: '',
-      iconUrl: '/images/tokens/yslc.svg',
-      type: 'interchain',
-      details: {
-        deployer: '0x25A967Ab86b379356AAf9Ad8264f78EAE82D5dc9',
-        deploySalt: '0x7a71c517e9782dcb798591899dc5ee04a0cd836e06ecd7edc7df5ba4ad658e14',
-        deploymentMessageId: '0xa415b4a4c28c71b37c59c49c3def35e447a6fce9c4f230cff8800ebd550a9faf-0',
-        originalMinter: ''
-      },
-  }*/
-    console.log(`Token ${tokenId} is missing in interchain_tokens, should be added:`, tokenInfo);
+     {
+        id: '0xc6b71ecb80f8f32ff4044b7c813f2af80029064883624fa7940b79ba3aaa864b',
+        prettySymbol: 'TEST',
+        name: 'Test',
+        decimals: 18,
+        originAxelarChainId: 'ethereum-sepolia',
+        coingeckoId: '',
+        iconUrl: '/images/tokens/sqd.svg',
+        type: 'interchain',
+        details: {
+          deployer: '0xba76c6980428A0b10CFC5d8ccb61949677A61233',
+          deploySalt: '0xdb508c07da6d2fa51f6bd2680fcd784c1400885e18bb2b1c2cc167267cb50229',
+          deploymentMessageId: '0x',
+          originalMinter: ''
+        },
+        chains: {
+          sui: {
+            tokenAddress: '0x3b09fe0f78b65b4d1c9dcf08d5a7a0f5d70246e9925f03f61ea3f4a954406c76::test::TEST',
+            symbol: 'TEST',
+            name: 'Test',
+            tokenManager: '0x9625c5709c95db082afb49c320cbca3cc0de2a929dbe6170f82193d2269d741a',
+            tokenManagerType: 'mintBurn',
+            decimals: 6
+          },
+          'ethereum-sepolia': {
+            tokenAddress: '0x6a3d312C98eF92d1520D7255A5b7d9B3C612B771',
+            symbol: 'TEST',
+            name: 'Test',
+            tokenManager: '0x23FD1014f7F11121f3FCd53bc0b8CF3bc130efe2',
+            tokenManagerType: 'lockUnlock',
+            decimals: 18
+          },
+          xrpl: {
+            tokenAddress: '5445535400000000000000000000000000000000.rNrjh1KGZk2jBR3wPfAQnoidtFFYQKbQn2',
+            symbol: 'TEST',
+            name: 'Test',
+            tokenManager: 'rNrjh1KGZk2jBR3wPfAQnoidtFFYQKbQn2',
+            tokenManagerType: 'mintBurn',
+            decimals: 15
+          }
+        }
+      }
+  */
     /*
     // attempt to get original chain by accessing tokenInfo.chains[tokenInfo.originAxelarChainId]
     let originalChain = tokenInfo.chains[tokenInfo.originAxelarChainId];
@@ -133,87 +168,99 @@ async function main() {
       tokenInfo.chains?.[tokenInfo.originAxelarChainId?.toLowerCase()];
     if (!originalChain) {
       console.error(
-        `Original chain ${tokenInfo.originAxelarChainId} not found in tokenInfo.chains for token ${tokenId}, skipping token`
+        `Original chain ${tokenInfo.originAxelarChainId} not found in tokenInfo.chains for token ${tokenId}, consider adding an extra rule`
       );
-      continue;
+      throw new Error();
     }
 
     // Use the correct fields from the example token structure
     try {
-      let result = await client.query(
+      await client.query(
         `INSERT INTO interchain_tokens
           (token_id, token_address, axelar_chain_id, token_name, token_symbol, token_decimals, deployment_message_id, deployer_address, token_manager_address, token_manager_type, original_minter_address, kind, salt, updated_at, created_at)
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, NOW(), NOW())
           RETURNING *`,
         [
-          tokenId,
-          originalChain.tokenAddress, // from the original chain object
-          tokenInfo.originAxelarChainId,
-          tokenInfo.name,
-          tokenInfo.prettySymbol,
-          tokenInfo.decimals, // decimals from the original chain object
-          tokenInfo.details?.deploymentMessageId ?? "0x",
-          tokenInfo.details?.deployer ?? null,
-          originalChain.tokenManager ?? null,
-          originalChain.tokenManagerType.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase() ?? null,
-          tokenInfo.details?.originalMinter ?? null,
-          tokenInfo.type,
-          tokenInfo.details?.deploySalt ?? "0x",
+          tokenId, // token_id
+          originalChain.tokenAddress, // token_address // from the original chain object
+          tokenInfo.originAxelarChainId, // axelar_chain_id
+          tokenInfo.name, // token_name
+          tokenInfo.prettySymbol, // token_symbol
+          tokenInfo.decimals, // token_decimals // decimals from the original chain object
+          tokenInfo.details?.deploymentMessageId ?? "0x", // deployment_message_id // if this doesn't exist, we still have to set it to 0x - otherwise the token will not show up as deployed
+          tokenInfo.details?.deployer ?? null, // deployer_address
+          originalChain.tokenManager ?? null, // token_manager_address
+          originalChain.tokenManagerType?.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase() ?? null, // token_manager_type
+          tokenInfo.details?.originalMinter ?? null, // original_minter_address
+          tokenInfo.type, // kind
+          tokenInfo.details?.deploySalt ?? "0x", // salt
+          // NOW() as created_at (there are no timestamps in the config)
+          // NOW() as updated_at (there are no timestamps in the config)
         ]
       );
     }
     catch (err) {
       console.error(`Error inserting token ${tokenId}:`, err);
-      continue;
+      throw err;
     }
+
+    tokensAdded.push(tokenId);
 
     // also add remote_interchain_tokens entries for each chain
     for (const [chainId, chainInfo] of Object.entries(tokenInfo.chains || {})) {
       if (chainId === tokenInfo.originAxelarChainId) continue; // skip original chain
 
       try { 
-        let result = await client.query(
-        `INSERT INTO remote_interchain_tokens
-          (id, token_id, axelar_chain_id, token_address, token_manager_address, token_manager_type, deployment_message_id, deployment_status, created_at, updated_at)
-          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW(), NOW())
-          RETURNING *`,
-        [
-          `${chainId}:${chainInfo.tokenAddress}`, // composite key
-          tokenId,
-          chainId,
-          chainInfo.tokenAddress,
-          chainInfo.tokenManager ?? null,
-          chainInfo.tokenManagerType.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase() ?? null,
-          tokenInfo.details?.deploymentMessageId ?? null,
-          'confirmed', // assume deployment confirmed if present in config
-        ]
-      );
+        await client.query(
+          `INSERT INTO remote_interchain_tokens
+            (id, token_id, axelar_chain_id, token_address, token_manager_address, token_manager_type, deployment_message_id, deployment_status, created_at, updated_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW(), NOW())
+            RETURNING *`,
+          [
+            `${chainId}:${chainInfo.tokenAddress}`, // id // composite key
+            tokenId, // token_id
+            chainId, // axelar_chain_id
+            chainInfo.tokenAddress, // token_address
+            chainInfo.tokenManager ?? null, // token_manager_address
+            chainInfo.tokenManagerType?.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase() ?? null, // token_manager_type
+            tokenInfo.details?.deploymentMessageId ?? null, // deployment_message_id
+            'confirmed', // deployment_status // assume deployment confirmed if present in config
+            // NOW() as created_at (there are no timestamps in the config)
+            // NOW() as updated_at (there are no timestamps in the config)
+          ]
+        );
       } catch (err) {
         console.error(`Error inserting remote token ${tokenId} for chain ${chainId}:`, err);
-        continue;
+        throw err;
       }
     }
   }
   await client.end();
 
-  //console.log("Fetched assets:", assets);
-  return null;
+  console.log("Tokens synced successfully");
 
-  // Insert tokens into DB (example table: xrpl_tokens)
-  for (const [tokenId, tokenInfo] of Object.entries(assets)) {
-    const { address } = tokenInfo as { address: string };
-    if (!address) continue;
-
-    await client.query(
-      `INSERT INTO xrpl_tokens (token_id, address)
-       VALUES ($1, $2)
-       ON CONFLICT (token_id) DO UPDATE SET address = EXCLUDED.address`,
-      [tokenId, address]
-    );
-    console.log(`Upserted token ${tokenId}: ${address}`);
+  console.log("\n🟢 Tokens Added Successfully:");
+  if (tokensAdded.length) {
+    tokensAdded.forEach(token => console.log(`- ${token}`));
+  } else {
+    console.log("-- None --");
   }
 
-  await client.end();
+  console.log("\n🟡 Tokens Ignored:");
+  if (tokensIgnored.length) {
+    tokensIgnored.forEach(token => console.log(`- ${token}`));
+  } else {
+    console.log("-- None --");
+  }
+
+  console.log("\n🔵 Tokens Already Present:");
+  if (tokensAlreadyPresent.length) {
+    tokensAlreadyPresent.forEach(token => console.log(`- ${token}`));
+  } else {
+    console.log("-- None --");
+  }
+
+  return;
 }
 
 main().catch((err) => {
