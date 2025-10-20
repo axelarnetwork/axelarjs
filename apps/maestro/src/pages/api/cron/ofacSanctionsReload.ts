f05254deabd7427f62493bf5ad6dfb856b2833f5
João Azevedo <joaoazevedo@ieee.org>
2025-10-20 08:46:23 -0500

    fix: only allow GET on endpoint used by cron

--- apps/maestro/src/pages/api/cron/ofacSanctionsReload.ts
+++ apps/maestro/src/pages/api/cron/ofacSanctionsReload.ts
@@ -7,8 +7,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    res.setHeader("Allow", "POST");
+  if (req.method !== "GET") {
+    res.setHeader("Allow", "GET");
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
