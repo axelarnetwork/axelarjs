b8645899b69b48522a16964168162bc8a17d94b2
Jason Athanasoglou <jason@commonprefix.com>
2025-10-22 17:26:46 +0300

    chore: bring back sentry config

--- apps/maestro/next.config.js
+++ apps/maestro/next.config.js
@@ -1,4 +1,5 @@
 import bundleAnalyzer from "@next/bundle-analyzer";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const HOSTNAMES = [
   "testnet.axelar.network",
@@ -80,35 +81,32 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-// Temporarily disable Sentry to see the actual build error
-export default withBundleAnalyzer(nextConfig);
+export default withSentryConfig(
+  withBundleAnalyzer(nextConfig),
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-// export default withSentryConfig(
-//   withBundleAnalyzer(nextConfig),
-//   {
-//     // For all available options, see:
-//     // https://github.com/getsentry/sentry-webpack-plugin#options
+    // Suppresses source map uploading logs during build
+    silent: true,
 
-//     // Suppresses source map uploading logs during build
-//     silent: true,
+    org: "axelar-network",
+    project: `interchain-maestro-${process.env.NEXT_PUBLIC_NETWORK_ENV}`,
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-//     org: "axelar-network",
-//     project: `interchain-maestro-${process.env.NEXT_PUBLIC_NETWORK_ENV}`,
-//   },
-//   {
-//     // For all available options, see:
-//     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-//     // Upload a larger set of source maps for prettier stack traces (increases build time)
-//     widenClientFileUpload: true,
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
 
-//     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-//     tunnelRoute: "/monitoring",
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 
-//     // Hides source maps from generated client bundles
-//     hideSourceMaps: true,
-
-//     // Automatically tree-shake Sentry logger statements to reduce bundle size
-//     disableLogger: true,
-//   }
-// );
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
